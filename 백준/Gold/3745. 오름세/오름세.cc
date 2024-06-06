ae331b45e983cb529d95bf6db1a10fc2f70195e0
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-06-07 07:32:49 +0900

    [Gold II] Title: 오름세, Time: 12 ms, Memory: 2924 KB -BaekjoonHub

---
+++ 백준/Gold/3745. 오름세/오름세.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[100001];
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n = 0;
+	while (cin >> n) {
+		vector<int>v(n);
+		for (int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		dp[0] = v[0];
+		int idx = 0;
+		for (int i = 1; i < n; i++) {
+			if (dp[idx] < v[i]) {
+				dp[++idx] = v[i];
+			}
+			else {
+				int start = 0, end = idx, mid = 0;
+				while (start < end) {
+					mid = (start + end) / 2;
+					if (dp[mid] >= v[i]) {
+						end = mid;
+					}
+					else {
+						start = mid + 1;
+					}
+				}
+				dp[end] = v[i];
+			}
+		}
+		cout << idx + 1 << '\n';
+		memset(dp, 0, sizeof(dp));
+	}
+	
+	return 0;
+}
