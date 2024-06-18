59459096d068bec4076942d6330bf82911b13c59
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-06-18 15:31:36 +0900

    [Gold V] Title: 집합의 표현, Time: 48 ms, Memory: 5416 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.c
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.c
@@ -2,44 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 int parent[1000001];
-int getParent(int x) {
-	if (parent[x] == x) {
-		return x;
+void init(int n) {
+	for (int i = 1; i <= n; i++) {
+		parent[i] = i;
 	}
-	return parent[x] = getParent(parent[x]);
 }
-int merge(int a, int b) {
+int getParent(int start) {
+	if (parent[start] == start) {
+		return start;
+	}
+	return parent[start] = getParent(parent[start]);
+}
+void merge(int a, int b) {
 	a = getParent(a);
 	b = getParent(b);
-	if (a < b) {
-		parent[b] = a;
-	}
-	else {
+	if (a > b) {
 		parent[a] = b;
 	}
-}
-int findParent(int a, int b) {
-	a = getParent(a);
-	b = getParent(b);
-	if (a == b) {
-		return 1;
+	else {
+		parent[b] = a;
 	}
-	return 0;
 }
 int main(void) {
 	int n = 0, m = 0;
 	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= n; i++) {
-		parent[i] = i;
-	}
+	init(n);
 	for (int i = 0; i < m; i++) {
 		int command = 0, start = 0, end = 0;
 		scanf("%d %d %d", &command, &start, &end);
 		if (command == 0) {
 			merge(start, end);
 		}
-		else if (command == 1) {
-			if (findParent(start, end) == 1) {
+		else {
+			if (getParent(start) == getParent(end)) {
 				printf("YES\n");
 			}
 			else {
