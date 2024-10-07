820d83035c8520f4f7d746954ca1864831f14b87
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 20:08:53 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.c
+++ 백준/Silver/2606. 바이러스/바이러스.c
@@ -1,59 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
-#include <stdbool.h>
-
-typedef struct Node
-{
-    int data;
-    struct Node* link;
+int visited[101];
+typedef struct Node {
+	int data;
+	struct Node* link;
 }Node;
-
 Node* graph[101];
-
-Node* insert(Node* head, int num)
-{
-    Node* new = (Node*)malloc(sizeof(Node));
-    new->data = num;
-    new->link = head;
-    head = new;
-    return head;
+Node* insert(Node* head, int num) {
+	Node* new = (Node*)malloc(sizeof(Node));
+	new->data = num;
+	new->link = head;
+	head = new;
+	return head;
 }
-void insertgraph(int start, int end)
-{
-    graph[start] = insert(graph[start], end);
-    graph[end] = insert(graph[end], start);
+void dfs(int start) {
+	visited[start] = 1;
+	for (Node* i = graph[start]; i != NULL; i = i->link) {
+		if (visited[i->data] == 0) {
+			dfs(i->data);
+		}
+	}
 }
-int ary[101] = { 0, };
-int count = 0;
-void dfs(int start)
-{
-    ary[start] = 1;
-    for (Node* i = graph[start]; i != NULL; i = i->link)
-    {
-        if (ary[i->data] == 0)
-        {        
-            count++;
-            ary[i->data] = 1;
-            dfs(i->data);
-        }
-    }
-}
-
-int main(void)
-{
-    int num1 = 0;
-    int num2 = 0;
-    scanf("%d", &num1);
-    scanf("%d", &num2);
-    for (int i = 0; i < num2; i++)
-    {
-        int start = 0;
-        int end = 0;
-        scanf("%d %d", &start, &end);
-        insertgraph(start, end);
-    }
-    dfs(1);
-    printf("%d", count);
+int main(void) {
+	int n = 0, m = 0;
+	scanf("%d", &n);
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++) {
+		int start = 0, end = 0;
+		scanf("%d %d", &start, &end);
+		graph[start] = insert(graph[start], end);
+		graph[end] = insert(graph[end], start);
+	}
+	dfs(1);
+	int count = 0;
+	for (int i = 1; i <= n; i++) {
+		if (visited[i] == 1) {
+			count++;
+		}
+	}
+	printf("%d", count - 1);
+	return 0;
 }
