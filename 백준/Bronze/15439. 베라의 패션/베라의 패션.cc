fc18112dd2805559faadc17a2c37e2064536922e
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 23:16:02 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n = 0; 
+	cin >> n;
+	cout << n * (n - 1);
+}
