1f8f60b14705ec5ce037ccd1d20ab510d3c64422
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 15:34:51 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.c
@@ -0,0 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+int compare(const void* num1, const void* num2) 
+{
+  int a = *(long long int*)num1;
+  int b = *(long long int*)num2;
+  if (a > b)
+  {
+	  return 1;
+  }
+  else if (a < b)
+  {
+	  return -1;
+  }
+  else
+  {
+	  return 0;
+  }
+
+}
+int search(int* ary, int size, int key)
+{
+	int start = 0, end = size - 1;
+	while (start <= end) 
+	{
+		int cen = (start + end) / 2;
+
+		if (ary[cen] == key)
+		{
+			return cen; 
+		}
+		else if (ary[cen] > key)
+		{
+			end = cen - 1;
+		}
+		else if (ary[cen] < key)
+		{
+			start = cen + 1;
+		}
+	}
+	return -1; 
+}
+
+int lower_bound(int* ary, int size, int key)
+{
+	int start = 0, end = size - 1;
+	while (start < end)
+	{
+		int cen = (start + end) / 2;
+		if (ary[cen] >= key)
+		{
+			end = cen;
+		}
+		else
+		{
+			start = cen + 1;
+		}
+	}
+	return end;
+}
+int upper_bound(int* ary, int size, int key)
+{
+	int start = 0, end = size - 1;
+	while (start < end)
+	{
+		int cen = (start + end) / 2;
+		if (ary[cen] > key)
+		{
+			end = cen;
+		}
+		else
+		{
+			start = cen + 1;
+		}
+	}
+	return end;
+}
+
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+
+	int result = 0;
+	scanf("%d", &num1);
+
+	int ary1[10] = { 64, 32, 16, 8, 4,2,1 };
+
+	for (int i = 0; i < 7; i++)
+	{
+		if (ary1[i] <= num1)
+		{
+			num2 = i;
+
+			break;
+		}
+
+	}
+
+	for (int i = num2; i < 7; i++)
+	{
+		if (num1 / ary1[i] >= 1)
+		{
+			result += num1 / ary1[i];
+			num1 -= (num1 / ary1[i]) * ary1[i];
+		}
+		
+
+	}
+	printf("%d", result);
+
+}
+
+	 
