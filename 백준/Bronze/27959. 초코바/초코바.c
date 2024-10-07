66661cf7526690cd3444aa574a8949f1bbe48b53
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:27:21 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int ary[10] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+
+char str[25];
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d %d", &num1, &num2);
+	if (num1 * 100 >= num2)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
+	
+}
