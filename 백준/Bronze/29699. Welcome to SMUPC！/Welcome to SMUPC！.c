0c98ab8d5bf3f9ee6b53203e559d155f73613732
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:32:10 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 0 ms, Memory: 1156 KB -BaekjoonHub

---
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int ary[10000] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+char str[115];
+int main(void)
+{
+	int num1 = 0;
+	scanf("%d", &num1);
+	strcpy(str, "WelcomeToSMUPC");
+
+
+	printf("%c", str[(num1 -1)% 14]);
+}
