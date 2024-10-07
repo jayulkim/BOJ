b1cbfb391193be2ad4977f9c5dca33002e592458
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:08:21 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int ary[5] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+char str[1000005];
+
+int main(void)
+{
+	long long int num1 = 0;
+	long long int num2 = 0;
+	scanf("%lld", &num1);
+	scanf("%lld", &num2);
+	printf("%lld\n",num2 - num1);
+	
+	
+}
