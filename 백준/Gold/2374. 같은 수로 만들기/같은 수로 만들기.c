eed76ba6f5db1f551ef257ca527efdb7493cf266
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 19:52:40 +0900

    [Gold IV] Title: 같은 수로 만들기, Time: 0 ms, Memory: 1132 KB -BaekjoonHub

---
+++ 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+long long int ary[1005] = { 0, };
+long long int stack[1005];
+int head = -1;
+
+int empty(long long int* stack)
+{
+	if (head == -1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void push(long long int* stack, long long int num)
+{
+	stack[++head] = num;
+}
+
+long long int pop(long long int* stack)
+{
+	return stack[head--];
+}
+
+long long int top(long long int* stack)
+{
+	return stack[head];
+}
+
+int main(void)
+{
+	int num1 = 0;
+	scanf("%d", &num1);
+	long long int max = 0;
+	for (int i = 0; i < num1; i++)
+	{
+		scanf("%d", &ary[i]);
+		if (max < ary[i])
+		{
+			max = ary[i];
+		}
+	}
+	long long int count = 0;
+	push(stack, ary[0]);
+	for (int i = 1; i < num1; i++)
+	{	
+		if (top(stack) <= ary[i])
+		{
+			count += (ary[i] - top(stack));
+			pop(stack);
+		}
+		else
+		{
+			pop(stack);
+		}
+		push(stack, ary[i]);
+	}
+
+	for (int i = 0; i <= head; i++)
+	{
+		count += (max - stack[i]);
+	}
+	
+	printf("%lld", count);
+}
