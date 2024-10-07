926e8e3a96ba56d80215124491badf21fb68adc9
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 16:34:26 +0900

    [Gold II] Title: 책정리, Time: 76 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Gold/1818. 책정리/책정리.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[200001];
+int main(void) {
+	int n = 0;
+	cin >> n;
+	vector<int>v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	dp[0] = v[0];
+	int idx = 0;
+	for (int i = 0; i < n; i++) {
+		if (dp[idx] < v[i]) {
+			dp[++idx] = v[i];
+		}
+		else {
+			int start = 0, end = idx, mid = 0;
+			while (start < end) {
+				mid = (start + end) / 2;
+				if (dp[mid] >= v[i]) {
+					end = mid;
+				}
+				else {
+					start = mid + 1;
+				}
+			}
+			dp[end] = v[i];
+		}
+	}
+	cout << n - idx - 1;
+ 	return 0;
+}
