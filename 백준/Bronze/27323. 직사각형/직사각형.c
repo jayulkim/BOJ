ca035588d59fb943a70406476de9c49cf0c754bf
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:24:20 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(void)
+{
+
+	int num1 = 0;
+	int num2 = 0;
+
+	scanf("%d", &num1);
+	scanf("%d", &num2);
+
+	printf("%d", num1 * num2);
+
+}
