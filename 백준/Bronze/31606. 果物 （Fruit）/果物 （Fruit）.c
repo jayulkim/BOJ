78ed426c72cdf31beb37d22444e585db42c96864
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:40:39 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num1 = 0;
+    int num2 = 0;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    printf("%d\n", num1 + num2 + 3);
+}
