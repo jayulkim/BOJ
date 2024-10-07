e67215aef15a3683d339661c80c6af6f38657531
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 22:02:37 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.cc
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+class antioverflow
+{
+public:
+	static ll addmod(ll x, ll y, ll num)
+	{
+		x %= num;
+		y %= num;
+		if (num - y <= x)
+		{
+			return x - (num - y);
+		}
+		else
+		{
+			return x + y;
+		}
+	}
+	static ll mulmod(ll x, ll y, ll num)
+	{
+		x %= num;
+		y %= num;
+		ll result = 0;
+		while (y > 0)
+		{
+			if (y % 2 == 1)
+			{
+				result = addmod(result, x, num);
+				result %= num;
+			}
+			x = addmod(x, x, num);
+			x %= num;
+			y /= 2;
+		}
+		return result;
+	}
+};
+class ary
+{
+public:
+	ary(ll num1)
+	{
+		n = num1;
+	}
+	ll** arymul(ll** x, ll** y, ll num)
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				x[i][j] %= num;
+				y[i][j] %= num;
+			}
+		}
+		ll** result = (ll**)malloc(sizeof(ll) * 2);
+		for (int i = 0; i < 2; i++)
+		{
+			result[i] = (ll*)malloc(sizeof(ll) * 2);
+		}	
+		for (int i = 0; i < 2; i++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				ll sum = 0;
+				for (int j = 0; j < 2; j++)
+				{
+					sum = antioverflow::addmod(sum, antioverflow::mulmod(x[i][j], y[j][k], num), num);
+					sum %= num;
+				}
+				result[i][k] = sum;
+				result[i][k] %= num;
+			}
+		}
+		return result;
+	}
+	ll** arypow(ll** x, ll num)
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				x[i][j] %= num;
+			}
+		}
+		ll** result = (ll**)malloc(sizeof(ll) * 2);
+		for (int i = 0; i < 2; i++)
+		{
+			result[i] = (ll*)malloc(sizeof(ll) * 2);
+		}
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				result[i][j] = x[i][j] % num;
+			}
+		}
+		n -= 1;
+		while (n > 0)
+		{
+			if (n % 2 == 1)
+			{
+				result = arymul(result, x, num);
+				for (int i = 0; i < 2; i++)
+				{
+					for (int j = 0; j < 2; j++)
+					{
+						result[i][j] %= num;
+					}
+				}
+			}
+			x = arymul(x, x, num);
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					x[i][j] %= num;
+				}
+			}
+			n /= 2;
+		}
+		return result;
+	}
+private:
+	ll n;
+};
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n = 0;
+	cin >> n;
+	ary solve1 = ary(n - 1);
+	ll** result1 = (ll**)malloc(sizeof(ll) * 2);
+	for (int i = 0; i < 2; i++)
+	{
+		result1[i] = (ll*)malloc(sizeof(ll) * 2);
+	}
+	result1[0][0] = 1;
+	result1[0][1] = 1;
+	result1[1][0] = 1;
+	result1[1][1] = 0;
+	ary solve2 = ary(n);
+	ll** result2 = (ll**)malloc(sizeof(ll) * 2);
+	for (int i = 0; i < 2; i++)
+	{
+		result2[i] = (ll*)malloc(sizeof(ll) * 2);
+	}
+	result2[0][0] = 1;
+	result2[0][1] = 1;
+	result2[1][0] = 1;
+	result2[1][1] = 0;
+	cout << antioverflow::mulmod(solve2.arypow(result2, 1000000007)[0][0], solve1.arypow(result1, 1000000007)[0][0], 1000000007) % 1000000007;
+}
