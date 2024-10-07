7f598bf2e1ff9714cd44607d3738aba21eeed73e
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:30:27 +0900

    [Bronze V] Title: 세제곱의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int ary[10] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+
+char str[25];
+int main(void)
+{
+	int num1 = 0;
+	
+	scanf("%d", &num1);
+	int sum = 0;
+	int sum1 = 0;
+	for (int i = 1; i <= num1; i++)
+	{
+		sum += i;
+		sum1 += i * i * i;
+	}
+	printf("%d\n", sum);
+	printf("%d\n", sum * sum);
+	printf("%d\n", sum1);
+	
+}
