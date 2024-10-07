4e6124d8b0154b221e309b787926679aaaf0a8d9
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:34:30 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 1152 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <memory.h>
+
+int ary[10000] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+char str1[10][100] = { {"Never gonna give you up"}, {"Never gonna let you down"}, {"Never gonna run around and desert you"}
+, {"Never gonna make you cry"}, {"Never gonna say goodbye"}, {"Never gonna tell a lie and hurt you"}, {"Never gonna stop"}, };
+
+char str[115];
+char str2[115];
+
+int main(void)
+{
+	double num1 = 0;
+	double num2 = 0;
+	scanf("%lf %lf", &num1, &num2);
+
+	printf("%.1lf", 0.5 * num1 * num2);
+}
