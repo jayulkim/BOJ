503bfae1e7c661fe690f092afc0423033a60b205
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 23:57:21 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int ary[5] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+char str[1000005];
+
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	int num4 = 0;
+	scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
+
+	printf("%d", 56 * num1 + 24 * num2 + 14 * num3 + 6 * num4);
+}
