405f50e465cf51fb3236dc4bb4783f1d1bd7182a
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 20:05:40 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.c
+++ 백준/Silver/2563. 색종이/색종이.c
@@ -1,35 +1,80 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int search(long long int* ary, int size, long long int key)
+{
+	int start = 0, end = size - 1;
+	while (start <= end)
+	{
+		int cen = (start + end) / 2;
+
+		if (ary[cen] == key)
+		{
+			return cen;
+		}
+		else if (ary[cen] > key)
+		{
+			end = cen - 1;
+		}
+		else if (ary[cen] < key)
+		{
+			start = cen + 1;
+		}
+	}
+	return -1;
+}
 
 int main(void)
 {
-	int num = 0;
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
 	int result = 0;
-	int ary[101][101] = { 0, };
-	scanf("%d", &num);
-	for(int i = 0; i< num; i++)
+	int ary1[100][100] = { 0, };
+
+	scanf("%d", &num1);
+
+	for (int i = 0; i < num1; i++)
 	{
-		int num1 = 0;
-		int num2 = 0;
-		scanf("%d %d", &num1, &num2);
-		for (int i = num1-1; i <= num1 + 8; i++)
+		
+
+		scanf("%d %d", &num2, &num3);
+
+		for (int j = num2; j < num2 + 10; j++)
 		{
-			for (int j = num2-1; j <= num2 + 8; j++)
+			for (int k = num3; k < num3 + 10; k++)
 			{
-				ary[i][j] = 1;
+				ary1[k][j] = 1;
 			}
 		}
 	}
-	for (int i = 0; i < 101; i++)
+
+	for (int i = 0; i < 100; i++)
 	{
-		for (int j = 0; j < 101; j++)
+		for (int j = 0; j < 100; j++)
 		{
-			if (ary[i][j] != 0)
-			{
-				result++;
-			}
+			result += ary1[j][i];
 		}
 	}
 	printf("%d", result);
-
 }
