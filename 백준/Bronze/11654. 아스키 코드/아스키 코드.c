e066068545d4d1bdd2e3cb2ed5423e85559799af
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 22:10:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+
+int main(void)
+{
+	char str;
+	scanf("%c", &str);
+	printf("%d", str);
+}
