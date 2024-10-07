bea4f736a140418208b896805a811ca8485c5e81
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 20:45:09 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(void)
+{
+	printf(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
+	
+}
