db46fe13b2b8632f7409daef79baae2b9645690a
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 23:21:56 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	int num1 = 0;
+
+	scanf("%d", &num1);
+
+	if (num1 == 0)
+	{
+		printf("YONSEI");
+	}
+	else if (num1 == 1)
+	{
+		printf("Leading the Way to the Future");
+	}
+
+}
