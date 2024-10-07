84b6e8efbce483e5da6f28e677ec42909db522fe
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 23:39:50 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+int ary[50] = { 0, };
+
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d %d", num2 - num1, num2);
+
+
+}
