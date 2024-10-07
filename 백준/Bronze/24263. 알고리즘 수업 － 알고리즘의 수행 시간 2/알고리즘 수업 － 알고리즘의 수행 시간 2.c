81b44f0be8bb496306b1342b3500e89c3529d0e9
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:08:41 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int compare(const void* num1, const void* num2) 
+{
+  int a = *(int*)num1;
+  int b = *(int*)num2;
+  if (a > b)
+  {
+	  return 1;
+  }
+  else if (a < b)
+  {
+	  return -1;
+  }
+  else
+  {
+	  return 0;
+  }
+
+}
+int lower_bound(int* ary, int size, int key)
+{
+	int start = 0, end = size - 1;
+	while (start < end)
+	{
+		int cen = (start + end) / 2;
+		if (ary[cen] >= key)
+		{
+			end = cen;
+		}
+		else
+		{
+			start = cen + 1;
+		}
+	}
+	return end;
+}
+int upper_bound(int* ary, int size, int key)
+{
+	int start = 0, end = size - 1;
+	while (start < end)
+	{
+		int cen = (start + end) / 2;
+		if (ary[cen] > key)
+		{
+			end = cen;
+		}
+		else
+		{
+			start = cen + 1;
+		}
+	}
+	return end;
+}
+
+int main(void)
+{
+	int num1 = 0;;
+
+	scanf("%d", &num1);
+
+	printf("%d\n1", num1);
+
+}
+	
