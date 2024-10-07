6a13f6efb3484c1ad1877a316a2fd3be5c9a0916
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 16:37:13 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+long long int ary[1000005] = { 0, };
+
+int main(void)
+{
+	int num1 = 0;
+	scanf("%d", &num1);
+
+	ary[0] = 1;
+	ary[1] = 2;
+
+	for (int i = 2; i < num1; i++)
+	{
+		ary[i] = ary[i - 1] + ary[i - 2];
+		ary[i] %= 15746;
+	}
+	printf("%lld\n", ary[num1 - 1] % 15746);
+
+}
