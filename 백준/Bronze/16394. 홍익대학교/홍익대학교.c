0f79c8732837f42f90976a40cf275ed100882aab
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 23:38:52 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 3064 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+int ary[500005] = { 0, };
+
+int main(void)
+{
+	int num1 = 0;
+	scanf("%d", &num1);
+	printf("%d\n", num1 - 1946);
+}
