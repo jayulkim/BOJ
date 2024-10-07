c40d753eda9ab542c8444a171bd052852405ea63
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 20:45:37 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(void)
+{
+	printf("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");
+	
+}
