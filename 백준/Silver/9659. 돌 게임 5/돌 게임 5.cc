8504151033bc896a1c8008ac9101902ead7c8438
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 21:19:01 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(void)
+{
+	ll n = 0;
+	cin >> n;
+	if (n % 2 == 0)
+	{
+		cout << "CY";
+	}
+	else
+	{
+		cout << "SK";
+	}
+}
