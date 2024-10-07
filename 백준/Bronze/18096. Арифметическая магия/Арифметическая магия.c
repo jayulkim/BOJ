58c3dee661bd3074e05064e890bb71f5994d4bd5
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 23:50:28 +0900

    [Bronze V] Title: Арифметическая магия, Time: 0 ms, Memory: 3068 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int ary[5] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+char str[1000005];
+char str1[1000005];
+int main(void)
+{
+	int num = 0;
+	scanf("%d", &num);
+	printf("1");
+	
+
+}
