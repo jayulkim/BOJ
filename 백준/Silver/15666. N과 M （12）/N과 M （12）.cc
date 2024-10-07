be0b90fca169100fd9081d1cc70f877e128aa724
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 23:21:41 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>graph;
+int n = 0, m = 0;
+vector<int>v;
+int idx = 0;
+void dfs() {
+	if (graph.size() == m) {
+		for (auto& i : graph) {
+			cout << i << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	int temp = 0;
+	for (int i = idx; i < v.size(); i++) {
+		if (v[i] != temp) {
+			graph.push_back(v[i]);
+			idx = i;
+			dfs();
+			temp = v[i];
+			graph.pop_back();
+		}
+	}
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int num = 0;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	dfs();
+	return 0;
+}
