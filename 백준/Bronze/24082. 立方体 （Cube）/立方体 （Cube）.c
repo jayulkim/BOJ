667c961442bf199f24e042ad506339fbcdb4c2cc
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:08:14 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int ary[5] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+char str[1000005];
+
+int main(void)
+{
+	long long int num1 = 0;
+	scanf("%lld", &num1);
+	printf("%lld\n", num1 * num1 * num1);
+	
+	
+}
