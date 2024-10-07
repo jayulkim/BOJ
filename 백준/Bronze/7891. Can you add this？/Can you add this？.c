40ce80877ba31ce0ceac58ad519da0e1036656de
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 21:02:17 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int ary[5] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+int main(void)
+{
+	int num1 = 0;
+	scanf("%d", &num1);
+	for (int i = 0; i < num1; i++)
+	{
+		int num2 = 0;
+		int num3 = 0;
+		scanf("%d %d", &num2, &num3);
+		printf("%d\n", num2 + num3);
+	}
+
+}
