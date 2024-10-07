df0267f6bfc43a844366650adf00f4b45cdb10ca
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 20:36:22 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(void)
+{
+	while (1)
+	{
+		int num1 = 0;
+		int num2 = 0;
+
+		scanf("%d %d", &num1, &num2);
+		if (num1 == 0 && num2 == 0)
+		{
+			break;
+		}
+		if (num1 > num2)
+		{
+			printf("Yes\n");
+		}
+		else if (num1 <= num2)
+		{
+			printf("No\n");
+		}
+		
+	}
+	
+}
