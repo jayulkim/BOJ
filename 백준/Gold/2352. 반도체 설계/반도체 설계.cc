68758fd3f948b6123b3b564e612c33e0a7fa289e
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 19:45:32 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2424 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[40001];
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n = 0;
+	cin >> n;
+	vector<int>v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	int idx = 0;
+	dp[0] = v[0];
+	for (int i = 1; i < n; i++) {
+		if (dp[idx] < v[i]) {
+			dp[++idx] = v[i];
+		}
+		else {
+			int start = 0;
+			int end = idx;
+			int mid = 0;
+			while (start < end) {
+				mid = (start + end) / 2;
+				if (dp[mid] >= v[i]) {
+					end = mid;
+				}
+				else {
+					start = mid + 1;
+				}
+			}
+			dp[end] = v[i];
+		}
+	}
+	cout << idx + 1;
+	return 0;
+}
