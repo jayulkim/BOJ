6e865530d09ced95af62fa41c806d7f9f6e0d790
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 20:09:30 +0900

    [Gold IV] Title: 구슬 찾기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+using namespace std;
+typedef long long ll;
+ll dist[101][101];
+bool result[101];
+void floydWarshall(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            for (int k = 1; k <= n; k++) {
+                dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
+            }
+        }
+    }
+}
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n = 0, m = 0;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            dist[i][j] = INF;
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        int start = 0, end = 0;
+        cin >> start >> end;
+        dist[start][end] = 1;
+    }
+    floydWarshall(n);
+    for (int i = 1; i <= n; i++) {
+        int count = 0;
+        for (int j = 1; j <= n; j++) {
+            if (dist[i][j] != INF) {
+                count++;
+            }
+        }
+        if (count >= (n + 1) / 2) {
+            result[i] = true;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        int count = 0;
+        for (int j = 1; j <= n; j++) {
+            if (dist[j][i] != INF) {
+                count++;
+            }
+        }
+        if (count >= (n + 1) / 2) {
+            result[i] = true;
+        }
+    }
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (result[i]) {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
