5e9b31c578f3013e93945b10f4b559ee0476752f
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 20:15:47 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	int num1 = 0;
+
+	scanf("%d", &num1);
+	
+	for (int i = 1; i <= num1; i++)
+	{
+		printf("%d\n", i);
+	}
+
+   
+}
