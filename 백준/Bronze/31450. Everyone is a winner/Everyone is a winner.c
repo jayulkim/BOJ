0917740cb3d81f950a882f7ba5e87d0bc44c682f
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:39:59 +0900

    [Bronze V] Title: Everyone is a winner, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d %d", &num1, &num2);
+	if (num1 % num2 == 0)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
+}
