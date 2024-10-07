9f6ae3c86def06e37e521fc757e1b6b31308da4b
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 21:47:30 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2112 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* num1, const void* num2) 
+{
+  int a = *(int*)num1;
+  int b = *(int*)num2;
+  if (a > b)
+  {
+	  return 1;
+  }
+  else if (a < b)
+  {
+	  return -1;
+  }
+  else
+  {
+	  return 0;
+  }
+
+}
+int lower_bound(int* ary, int size, int key)
+{
+	int start = 0, end = size - 1;
+	while (start < end)
+	{
+		int cen = (start + end) / 2;
+		if (ary[cen] >= key)
+		{
+			end = cen;
+		}
+		else
+		{
+			start = cen + 1;
+		}
+	}
+	return end;
+}
+int upper_bound(int* ary, int size, int key)
+{
+	int start = 0, end = size - 1;
+	while (start < end)
+	{
+		int cen = (start + end) / 2;
+		if (ary[cen] > key)
+		{
+			end = cen;
+		}
+		else
+		{
+			start = cen + 1;
+		}
+	}
+	return end;
+}
+
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+
+	int count = 0;
+	scanf("%d", &num1);
+	int ary1[10001] = { 0, };
+
+	
+	for (int i = 0; i < num1; i++)
+	{
+		scanf("%d", &num2);
+		ary1[num2]++;
+	}
+	for (int i = 1; i <= 10000; i++)
+	{
+		if (ary1[i] != 0)
+		{
+			for (int j = 0; j < ary1[i]; j++)
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+
+}
+	
