0f94f38d86fe117498e389a383129dace946e17a
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-08 00:08:05 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int ary[5] = { 0, };
+
+int compare(const void* num1, const void* num2)
+{
+	int a = *(int*)num1;
+	int b = *(int*)num2;
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a < b)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+char str[1000005];
+
+int main(void)
+{
+	int num1 = 0;
+	scanf("%d", &num1);
+	printf("%d\n", num1 % 21);
+	
+	
+}
