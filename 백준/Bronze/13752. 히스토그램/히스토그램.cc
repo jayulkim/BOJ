b225b2de228f6683fb7cf65093b70d4d1572b3fc
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-07 22:49:25 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int m = 0;
+		cin >> m;
+		for (int j = 0; j < m; j++)
+		{
+			cout << '=';
+		}
+		cout << '\n';
+	}
+}
