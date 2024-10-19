129a302383101fc06cd3061d5a964cc0c35d4b42
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-20 06:40:00 +0900

    [Gold IV] Title: N-Queen, Time: 704 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef int ll;
 ll n = 0;
-ll up[15], ary[60];
+bool up[15], a[30], b[30];
 ll result = 0;
 void dfs(ll temp) {
 	if (temp == n) {
@@ -10,14 +10,14 @@ void dfs(ll temp) {
 		return;
 	}
 	for (int i = 0; i < n; i++) {
-		if (!up[i] && !ary[i + temp] && !ary[3 * n - i + temp]) {
-			up[i]++;
-			ary[i + temp]++;
-			ary[3 * n - i + temp]++;
+		if (up[i] + a[i + temp] + b[n - i + temp] == 0) {
+			up[i] = true;
+			a[i + temp] = true;
+			b[n - i + temp] = true;
 			dfs(temp + 1);
-			up[i]--;
-			ary[i + temp]--;
-			ary[3 * n - i + temp]--;
+			up[i] = false;
+			a[i + temp] = false;
+			b[n - i + temp] = false;
 		}
 	}
 }
