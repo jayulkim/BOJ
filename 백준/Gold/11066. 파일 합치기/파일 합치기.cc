52d7f15a8ef679fd9bad641b293816306d444dbd
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-24 19:14:40 +0900

    [Gold III] Title: 파일 합치기, Time: 844 ms, Memory: 4124 KB -BaekjoonHub

---
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int  T = 0;
+	cin >> T;
+	while (T--)
+	{
+		int n = 0;
+		cin >> n;
+		vector<ll>v(n + 1, 0);
+		vector<ll>prefix(n + 1, 0);
+		for (int i = 1; i <= n; ++i)
+		{
+			cin >> v[i];
+			prefix[i] = prefix[i - 1] + v[i];
+		}
+		vector<vector<ll>>dp(n + 1, vector<ll>(n + 1, -1));
+		function<ll(int, int)> dfs = [&](int left, int right)
+			{
+				if (left == right)
+				{
+					return 0LL;
+				}
+				if (dp[left][right] != -1)
+				{
+					return dp[left][right];
+				}
+				ll cost = LLONG_MAX;
+				for (int i = left; i < right; ++i)
+				{
+					cost = min(cost, dfs(left, i) + dfs(i + 1, right) + prefix[right] - prefix[left - 1]);
+				}
+				return dp[left][right] = cost;
+			};
+		cout << dfs(1, n) << '\n';
+	}
+	return 0;
+}
