009ab8089f9803e3dd2988d48e51de366c8384e6
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-26 12:00:46 +0900

    [Platinum V] Title: 선분 교차 EX, Time: 212 ms, Memory: 6240 KB -BaekjoonHub

--- 백준/Platinum/27718. 선분 교차 EX/선분 교차 EX.cc
+++ 백준/Platinum/27718. 선분 교차 EX/선분 교차 EX.cc
@@ -6,7 +6,7 @@ typedef pair<ld, ld> pld;
 pld vec(ld x1, ld y1, ld x2, ld y2) {
 	return { x2 - x1, y2 - y1 };
 }
-ld ccw(pld p1, pld p2, pld p3) {
+ll ccw(pld p1, pld p2, pld p3) {
 	pld a = vec(p1.first, p1.second, p2.first, p2.second);
 	pld b = vec(p2.first, p2.second, p3.first, p3.second);
 	ld temp = a.first * b.second - a.second * b.first;
@@ -21,65 +21,37 @@ ld ccw(pld p1, pld p2, pld p3) {
 	}
 }
 char cross(pld start1, pld end1, pld start2, pld end2) {
-	if (start1 > end1) {
-		swap(start1, end1);
-	}
-	if (start2 > end2) {
-		swap(start2, end2);
-	}
-	ld a = ccw(start1, end1, start2);
-	ld b = ccw(start1, end1, end2);
-	ld c = ccw(start2, end2, start1);
-	ld d = ccw(start2, end2, end1);
-	if (a * b == 0 && c * d == 0) {
-		if (start1 <= end2 && start2 <= end1) {
-			if (start1.first == end1.first && start2.first == end2.first) {
-				if (start1 > start2) {
-					swap(start1, start2);
-					swap(end1, end2);
-				}
-				if (start1.first == start2.first && start2 == end1) {
-					return '1';
-				}
-				return '3';
-			}
-			else if ((end2.second - start2.second) * (end1.first - start1.first) == (end1.second - start1.second) * (end2.first - start2.first)) {
-				if (start1 > start2) {
-					swap(start1, start2);
-					swap(end1, end2);
-				}
-				if (start2 == end1) {
-					return '1';
-				}
-				return '3';
-			}
-			else {
-				if (start1 == start2 || start1 == end2) {
-					return '1';
-				}
-				if (end1 == start2 || end1 == end2) {
-					return '1';
-				}
-				return '3';
-			}
-		}
-		else {
-			return '0';
-		}
-	}
-	if (a * b <= 0 && c * d <= 0) {
-		if (start1 > start2) {
-			swap(start1, start2);
-			swap(end1, end2);
-		}
-		if (!ccw(start1, end1, start2) || !ccw(start1, end1, end2) || !ccw(start1, start2, end2) || !ccw(end1, start2, end2)) {
-			return '1';
-		}
-		return '2';
-	}
-	else {
+	ll a = ccw(start1, end1, start2);
+	ll b = ccw(start1, end1, end2);
+	ll c = ccw(start2, end2, start1);
+	ll d = ccw(start2, end2, end1);
+	ll e = a * b;
+	ll f = c * d;
+	if (e > 0 || f > 0) {
 		return '0';
 	}
+	if (e < 0 && f < 0) {
+		return '2';
+	}
+	if (!e && !f) {
+		if (a || b || c || d) {
+			return '1';
+		}
+		if (end1 < start1) {
+			swap(start1, end1);
+		}
+		if (end2 < start2) {
+			swap(start2, end2);
+		}
+		if (end1 < start2 || end2 < start1) {
+			return '0';
+		}
+		if (end1 == start2 || start1 == end2) {
+			return '1';
+		}
+		return '3';
+	}
+	return '1';
 }
 int main(void) {
 	ios::sync_with_stdio(false);
