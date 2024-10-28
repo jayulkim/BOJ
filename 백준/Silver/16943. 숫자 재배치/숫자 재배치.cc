aac43bd227f29b0ff340d881d8fdf4146e6dde51
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-10-28 11:05:16 +0900

    [Silver I] Title: 숫자 재배치, Time: 56 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n = 0;
+string str = "";
+string result = "";
+bool visited[11];
+ll Max = 0;
+bool Find = false;
+void dfs() {
+	if (result.length() == str.length()) {
+		if (result.back() != '0') {
+			string temp = result;
+			reverse(temp.begin(), temp.end());
+			if (stoll(temp) < n) {
+				Max = max(stoll(temp), Max);
+				Find = true;
+			}
+		}
+		return;
+	}
+	for (int i = 0; i < str.length(); i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			result.push_back(str[i]);
+			dfs();
+			visited[i] = false;
+			result.pop_back();
+		}
+	}
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> str >> n;
+	dfs();
+	if (!Find) {
+		cout << -1;
+	}
+	else {
+		cout << Max;
+	}
+	return 0;
+}
