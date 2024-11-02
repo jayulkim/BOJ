71dc9e03b697393d4f2e37be1ddc238653530802
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-02 19:15:45 +0900

    [Silver I] Title: 아보와 킨텍스, Time: 0 ms, Memory: 2648 KB -BaekjoonHub

---
+++ 백준/Silver/32206. 아보와 킨텍스/아보와 킨텍스.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+map<string, ll>Map;
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n = 0;
+	cin >> n;
+	string str = "";
+	cin >> str;
+	cout << 26 + 25 * str.length();
+	return 0;
+}
