193b1f893197f2624069b30ea528bb6e4aed0f1c
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-06 23:50:52 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 144 ms, Memory: 4136 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+vector<pll>v;
+vector<vector<ll>>dp(501, vector<ll>(501, -1));
+ll dfs(ll left, ll right) {
+	if (left == right) {
+		return 0;
+	}
+	if (dp[left][right] != -1) {
+		return dp[left][right];
+	}
+	ll weight = LLONG_MAX;
+	for (int i = left; i < right; i++) {
+
+		weight = min(weight, dfs(left, i) + dfs(i + 1, right) + v[left].first * v[i].second * v[right].second);
+	}
+	return dp[left][right] = weight;
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		ll a = 0, b = 0;
+		cin >> a >> b;
+		v.push_back({ a, b });
+	}
+	cout << dfs(0, n - 1);
+	return 0;
+}
