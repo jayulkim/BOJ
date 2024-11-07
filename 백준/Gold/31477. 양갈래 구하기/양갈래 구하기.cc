f616ee1d41f5f10b73a7ba44ab0f60c2d9376cce
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-07 21:13:25 +0900

    [Gold III] Title: 양갈래 구하기, Time: 60 ms, Memory: 12740 KB -BaekjoonHub

--- 백준/Gold/31477. 양갈래 구하기/양갈래 구하기.cc
+++ 백준/Gold/31477. 양갈래 구하기/양갈래 구하기.cc
@@ -3,58 +3,36 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> pll;
 ll n = 0, m = 0, k = 0;
-vector<ll>graph[100001];
-vector<ll>dp(100001, -1);
+vector<pll>graph[100001];
 bool visited[100001];
-ll parent[100001];
-bool notleaf[100001];
-map<vector<ll>, ll>Map;
-void maketree(ll x) {
-	visited[x] = true;
-	for (auto& i : graph[x]) {
-		if (!visited[i]) {
-			parent[i] = x;
-			maketree(i);
-		}
-	}
-}
 ll dfs(ll node) {
-	if (visited[node]) {
-		return 0;
-	}
 	visited[node] = true;
+	if (graph[node].size() == 1) {
+		return graph[node][0].second;
+	}
 	ll cost = 0;
 	for (auto& i : graph[node]) {
-		if (!notleaf[i]) {
-			cost += Map[{i, parent[i]}];
-		}
-		else {
-			cost += dfs(i);
+		if (!visited[i.first]) {
+			cost += min(dfs(i.first), i.second);
 		}
 	}
-	return dp[node] = min(cost, Map[{node, parent[node]}]);
+	return cost;
 }
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		parent[i] = i;
-	}
 	for (int i = 0; i < n - 1; i++) {
 		ll start = 0, end = 0, weight = 0;
 		cin >> start >> end >> weight;
-		graph[start].push_back(end);
-		graph[end].push_back(start);
-		Map[{start, end}] = weight;
-		Map[{end, start}] = weight;
+		graph[start].push_back({ end, weight });
+		graph[end].push_back({ start, weight });
 	}
-	maketree(1);
-	Map[{1, 1}] = LLONG_MAX;
-	for (int i = 1; i <= n; i++) {
-		notleaf[parent[i]] = true;
+	ll result = 0;
+	visited[1] = true;
+	for (auto& i : graph[1]) {
+		result += min(dfs(i.first), i.second);
 	}
-	memset(visited, false, sizeof(visited));
-	cout << dfs(1);
+	cout << result;
 	return 0;
 }
