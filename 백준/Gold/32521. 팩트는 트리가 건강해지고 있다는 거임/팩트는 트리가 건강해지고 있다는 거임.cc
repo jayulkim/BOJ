e400532dc98c6f5c57bd475b7debebe3ef9dd9f4
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-08 20:51:25 +0900

    [Gold III] Title: 팩트는 트리가 건강해지고 있다는 거임, Time: 52 ms, Memory: 11724 KB -BaekjoonHub

---
+++ 백준/Gold/32521. 팩트는 트리가 건강해지고 있다는 거임/팩트는 트리가 건강해지고 있다는 거임.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0;
+vector<ll>v;
+vector<ll>graph[100001];
+ll result = 0;
+ll dfs(ll parent, ll node) {
+	vector<ll>temp;
+	ll count = v[node - 1];
+	for (auto& i : graph[node]) {
+		if (parent != i) {
+			temp.push_back(dfs(node, i));
+		}
+	}
+	sort(temp.begin(), temp.end());
+	for (int i = 0; i < temp.size(); ++i)
+	{
+		ll a = count + temp[i];
+		if (a > m)
+		{
+			result += temp.size() - i;
+			break;
+		}
+		count = a;
+	}
+	return count;
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		ll num = 0;
+		cin >> num;
+		v.push_back(num);
+	}
+	for (int i = 0; i < n - 1; i++) {
+		ll start = 0, end = 0;
+		cin >> start >> end;
+		graph[start].push_back(end);
+		graph[end].push_back(start);
+	}
+	dfs(-1, 1);
+	cout << result;
+	return 0;
+}
