c4aff067c23677cdeaa8c311b14125b70cd226fe
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-14 06:00:06 +0900

    [Gold II] Title: 횃불이 키우기, Time: 112 ms, Memory: 89612 KB -BaekjoonHub

---
+++ 백준/Gold/32378. 횃불이 키우기/횃불이 키우기.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> tp;
+typedef pair<ll, ll> pll;
+#define num 1e11
+ll n = 0, m = 0, k = 0;
+vector<vector<ll>>dp(200001, vector<ll>(51, -(1LL << 52)));
+vector<ll>v(200001);
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; i++) {
+		cin >> v[i];
+	}
+	if (m > 50) {
+		cout << "MEGA";
+		return 0;
+	}
+	dp[0][0] = k;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+			if (!j) {
+				if (dp[i - 1][j] <= (1LL << 52) - v[i] && dp[i - 1][j] + v[i] > 0) {
+					dp[i][j] = dp[i - 1][j] + v[i];
+				}
+				else if (dp[i - 1][j] > (1LL << 52) - v[i]) {
+					cout << "MEGA";
+					return 0;
+				}
+			}
+			else {
+				if (dp[i - 1][j] <= (1LL << 52) - v[i] && dp[i - 1][j] + v[i] > 0) {
+					dp[i][j] = dp[i - 1][j] + v[i];
+				}
+				else if (dp[i - 1][j] > (1LL << 52) - v[i]) {
+					cout << "MEGA";
+					return 0;
+				}
+				if (dp[i - 1][j - 1] <= (1LL << 52) - dp[i - 1][j - 1] && dp[i - 1][j - 1] * 2 > 0) {
+					dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] * 2);
+				}
+				else if (dp[i - 1][j - 1] > (1LL << 52) - dp[i - 1][j - 1]) {
+					cout << "MEGA";
+					return 0;
+				}
+			}
+			if (dp[i][j] > (1LL << 52)) {
+				cout << "MEGA";
+				return 0;
+			}
+		}
+	}
+	ll Max = 0;
+	for (int i = 0; i <= m; i++) {
+		Max = max(Max, dp[n][i]);
+	}
+	if (Max <= 0) {
+		cout << -1;
+		return 0;
+	}
+	if (Max > num) {
+		cout << "MEGA";
+		return 0;
+	}
+	cout << Max;
+	return 0;
+}
