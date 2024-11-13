393a9887c60b765a2da0ab34b8c07516c1d6734d
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-14 06:12:15 +0900

    [Gold II] Title: 횃불이 키우기, Time: 92 ms, Memory: 70860 KB -BaekjoonHub

--- 백준/Gold/32378. 횃불이 키우기/횃불이 키우기.cc
+++ 백준/Gold/32378. 횃불이 키우기/횃불이 키우기.cc
@@ -6,7 +6,7 @@ typedef tuple<ll, ll, ll> tp;
 typedef pair<ll, ll> pll;
 #define num 1e11
 ll n = 0, m = 0, k = 0;
-vector<vector<ll>>dp(200001, vector<ll>(51, -(1LL << 52)));
+vector<vector<ll>>dp(200001, vector<ll>(38, -(1LL << 38)));
 vector<ll>v(200001);
 int main(void) {
 	ios::sync_with_stdio(false);
@@ -15,51 +15,41 @@ int main(void) {
 	for (int i = 1; i <= n; i++) {
 		cin >> v[i];
 	}
-	if (m > 50) {
+	if (m > 37) {
 		cout << "MEGA";
 		return 0;
 	}
 	dp[0][0] = k;
 	for (int i = 1; i <= n; i++) {
+		ll temp = 0;
 		for (int j = 0; j <= m; j++) {
 			if (!j) {
-				if (dp[i - 1][j] <= (1LL << 52) - v[i] && dp[i - 1][j] + v[i] > 0) {
+				if (dp[i - 1][j] + v[i] > 0) {
 					dp[i][j] = dp[i - 1][j] + v[i];
-				}
-				else if (dp[i - 1][j] > (1LL << 52) - v[i]) {
-					cout << "MEGA";
-					return 0;
 				}
 			}
 			else {
-				if (dp[i - 1][j] <= (1LL << 52) - v[i] && dp[i - 1][j] + v[i] > 0) {
+				if (dp[i - 1][j] + v[i] > 0) {
 					dp[i][j] = dp[i - 1][j] + v[i];
 				}
-				else if (dp[i - 1][j] > (1LL << 52) - v[i]) {
-					cout << "MEGA";
-					return 0;
-				}
-				if (dp[i - 1][j - 1] <= (1LL << 52) - dp[i - 1][j - 1] && dp[i - 1][j - 1] * 2 > 0) {
+				if (dp[i - 1][j - 1] * 2 > 0) {
 					dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] * 2);
 				}
-				else if (dp[i - 1][j - 1] > (1LL << 52) - dp[i - 1][j - 1]) {
-					cout << "MEGA";
-					return 0;
-				}
 			}
-			if (dp[i][j] > (1LL << 52)) {
+			if (dp[i][j] > (1LL << 38)) {
 				cout << "MEGA";
 				return 0;
 			}
+			temp = max(temp, dp[i][j]);
+		}
+		if (temp <= 0) {
+			cout << -1;
+			return 0;
 		}
 	}
 	ll Max = 0;
 	for (int i = 0; i <= m; i++) {
 		Max = max(Max, dp[n][i]);
-	}
-	if (Max <= 0) {
-		cout << -1;
-		return 0;
 	}
 	if (Max > num) {
 		cout << "MEGA";
