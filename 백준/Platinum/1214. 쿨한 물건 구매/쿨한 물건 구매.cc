a36ec84029048cceea41b7771d89eabd82d695ae
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-16 07:46:32 +0900

    [Platinum V] Title: 쿨한 물건 구매, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> tp;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+map<ll, ll>Map;
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m >> k;
+	ll Min = LLONG_MAX;
+	if (m < k) {
+		swap(m, k);
+	}
+	for (int i = 0; i <= n / m + 1; i++) {
+		ll temp = m * i + ((n - m * i + k - 1) / k) * k;
+		if (Map[temp]) {
+			break;
+		}
+		Map[temp] = i;
+		Min = min(temp, Min);
+	}
+	cout << Min;
+	return 0;
+}
