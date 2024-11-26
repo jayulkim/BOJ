aa9bf33502bbd504ac1d428991beca7bdb5490d3
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-11-26 15:15:00 +0900

    [Silver II] Title: 타일링, Time: 100 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -0,0 +1,12 @@
+dp = [0 for i in range(0, 251)]
+dp[0] = 1
+dp[1] = 1
+dp[2] = 3
+for i in range(3, 251):
+    dp[i] = dp[i - 1] + 2 * dp[i - 2]
+while (True):
+    try:
+        a = int(input())
+        print(dp[a])
+    except:
+        break
