7d9a8e6eca8184e77034ffb19bb1186a11ba2ad6
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-12-21 23:35:28 +0900

    [Gold III] Title: 두 수의 곱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14279. 두 수의 곱/두 수의 곱.cc
+++ 백준/Gold/14279. 두 수의 곱/두 수의 곱.cc
@@ -9,14 +9,9 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> n >> m >> k;
-	if (n > m) {
-		swap(n, m);
-	}
 	ll Min = LLONG_MAX;
-	for (ll i = 1; i * i <= n + n + m + k - 3; i++) {
-		for (ll j = 1; j * i <= k + n + m + k - 3; j++) {
-			Min = min({ Min, llabs(n - i) + llabs(m - j) + llabs(k - i * j), llabs(n - j) + llabs(m - i) + llabs(k - i * j) });
-		}
+	for (ll i = 1; i * i <= k + n + m + k - 3; i++) {
+		Min = min({ Min, llabs(n - (max((ll)1, k / i))) + llabs(m - i) + llabs(k - i * (max((ll)1, k / i))), llabs(m - (max((ll)1, k / i))) + llabs(n - i) + llabs(k - i * (max((ll)1, k / i))), llabs(n - (max((ll)1, k / i + 1))) + llabs(m - i) + llabs(k - i * (max((ll)1, k / i + 1))), llabs(m - (max((ll)1, k / i + 1))) + llabs(n - i) + llabs(k - i * (max((ll)1, k / i + 1))) });
 	}
 	cout << Min;
 	return 0;
