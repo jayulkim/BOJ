d5128a93cbc88842c61806c223e91250b6ddc251
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-12-21 23:07:05 +0900

    [Gold III] Title: 두 수의 곱, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14279. 두 수의 곱/두 수의 곱.cc
+++ 백준/Gold/14279. 두 수의 곱/두 수의 곱.cc
@@ -9,9 +9,12 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> n >> m >> k;
+	if (n > m) {
+		swap(n, m);
+	}
 	ll Min = LLONG_MAX;
-	for (int i = 1; i <= 5000000; i++) {
-		for (int j = 1; j * i <= 5000000; j++) {
+	for (ll i = 1; i * i <= n + n + m + k - 3; i++) {
+		for (ll j = 1; j * i <= k + n + m + k - 3; j++) {
 			Min = min({ Min, llabs(n - i) + llabs(m - j) + llabs(k - i * j), llabs(n - j) + llabs(m - i) + llabs(k - i * j) });
 		}
 	}
