188e3226aa705e8ec317bb69e1db1d4ba5537f1f
jayulkim <144874950+jayulkim@users.noreply.github.com>
2024-12-21 22:38:28 +0900

    [Gold III] Title: 두 수의 곱, Time: 352 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/14279. 두 수의 곱/두 수의 곱.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> tp;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m >> k;
+	ll Min = LLONG_MAX;
+	for (int i = 1; i <= 10000000; i++) {
+		for (int j = 1; j * i <= 10000000; j++) {
+			Min = min({ Min, llabs(n - i) + llabs(m - j) + llabs(k - i * j), llabs(n - j) + llabs(m - i) + llabs(k - i * j) });
+		}
+	}
+	cout << Min;
+	return 0;
+}
