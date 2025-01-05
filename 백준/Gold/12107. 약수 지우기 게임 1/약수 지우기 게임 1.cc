a7bf7af6af72d3fddd82bfe2e08e7e904b9e31a4
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-01-05 15:10:54 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> tp;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n;
+	if (n < 2) {
+		cout << 'B';
+	}
+	else {
+		cout << 'A';
+	}
+	return 0;
+}
