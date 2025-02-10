ed615cee7a548a8e080fbbe178e8a9c3502e9a93
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-02-10 22:45:58 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 12 ms, Memory: 9876 KB -BaekjoonHub

--- 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.cc
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.cc
@@ -12,21 +12,21 @@ vector<vector<ll>>dp(1001, vector<ll>(1001, 0));
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(0);
+    dp[1][1] = 1;
+    dp[2][1] = 1;
+    dp[2][2] = 1;
+    dp[3][1] = 1;
+    dp[3][2] = 2;
+    dp[3][3] = 1;
+    for (int i = 4; i <= 1000; i++) {
+        for (int j = 1; j <= 1000; j++) {
+            dp[i][j] = (dp[i - 1][j - 1] % mod + dp[i - 2][j - 1] % mod + dp[i - 3][j - 1] % mod) % mod;
+        }
+    }
     ll num = 0;
     cin >> num;
     while (num--) {
         cin >> n >> m;
-        dp[1][1] = 1;
-        dp[2][1] = 1;
-        dp[2][2] = 1;
-        dp[3][1] = 1;
-        dp[3][2] = 2;
-        dp[3][3] = 1;
-        for (int i = 4; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                dp[i][j] = (dp[i - 1][j - 1] % mod + dp[i - 2][j - 1] % mod + dp[i - 3][j - 1] % mod) % mod;
-            }
-        }
         ll result = 0;
         for (int i = 1; i <= m; i++) {
             result += dp[n][i] % mod;
