c12d75a48fef433b4ee358bcef277f3300ff105a
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-02-13 22:03:56 +0900

    [Platinum V] Title: 히스토그램, Time: 12 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<ll, string> pls;
+typedef pair<string, ll> psl;
+typedef tuple<ll, ll, ll>tp;
+typedef tuple<pll, string, ll>pllsl;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    vector<ll>v;
+    for (int i = 0; i < n; i++) {
+        ll num = 0;
+        cin >> num;
+        v.push_back(num);
+    }
+    v.push_back(-1);
+    ll Max = 0;
+    vector<ll>stack;
+    for (int i = 0; i <= n; i++) {
+        while (!stack.empty() && v[stack.back()] >= v[i]) {
+            ll temp = stack.back();
+            stack.pop_back();
+            if (!stack.empty()) {
+                Max = max(Max, v[temp] * (i - 1 - stack.back()));
+            }
+            else {
+                Max = max(Max, v[temp] * i);
+            }
+        }
+        stack.push_back(i);
+    }
+    cout << Max;
+    return 0;
+}
