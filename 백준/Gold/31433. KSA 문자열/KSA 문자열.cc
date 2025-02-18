53e92f8135d16aa66af3e3f5db53a893305daf72
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-02-18 21:56:50 +0900

    [Gold IV] Title: KSA 문자열, Time: 12 ms, Memory: 3184 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
+++ 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
@@ -15,7 +15,7 @@ int main(void) {
     cin >> str;
     deque<char>dq;
     ll Min = LLONG_MAX;
-    for (int i = 0; i < str.length(); i++) {
+    for (int i = 0; i < str.length() + 3; i++) {
         if (i % 3 == 0) {
             dq.push_back('K');
         }
@@ -28,7 +28,7 @@ int main(void) {
     }
     ll idx = 0;
     ll count = 0;
-    for (int i = 0; i < str.length(); i++) {
+    for (int i = 0; i < str.length() && idx < str.length(); i++) {
         if (str[i] == dq[idx]) {
             count++;
             idx++;
@@ -39,24 +39,34 @@ int main(void) {
     idx = 0;
     dq.push_back(dq.front());
     dq.pop_front();
-    for (int i = 0; i < str.length(); i++) {
+    for (int i = 0; i < str.length() && idx < str.length(); i++) {
         if (str[i] == dq[idx]) {
             count++;
             idx++;
         }
     }
-    Min = min(Min, 2 * ((ll)str.length() - count));
+    if (str.length() - count >= 2) {
+        Min = min(Min, 2 * ((ll)str.length() - count));
+    }
+    else {
+        Min = min(Min, (ll)2);
+    }
     count = 0;
     idx = 0;
     dq.push_back(dq.front());
     dq.pop_front();
-    for (int i = 0; i < str.length(); i++) {
+    for (int i = 0; i < str.length() && idx < str.length(); i++) {
         if (str[i] == dq[idx]) {
             count++;
             idx++;
         }
     }
-    Min = min(Min, 2 * ((ll)str.length() - count));
+    if (str.length() - count >= 4) {
+        Min = min(Min, 2 * ((ll)str.length() - count));
+    }
+    else {
+        Min = min(Min, (ll)4);
+    }
     cout << Min;
     return 0;
 }
