eff726924c559f6ab570d0413b1dae130f762b78
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-02-20 21:01:05 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -1,42 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-bool visited[1000001];
-ll soinsoo[1000001];
-ll result = 1;
-void eulerphi(ll n) {
-    for (int i = 2; i <= n / i; i++) {
-        if (soinsoo[i]) {
-            for (int j = 0; j < soinsoo[i] - 1; j++) {
+typedef pair<ll, ll> pll;
+typedef pair<ll, string> pls;
+typedef pair<string, ll> psl;
+typedef tuple<ll, ll, ll>tp;
+typedef tuple<pll, string, ll>pllsl;
+ll n = 0, m = 0, k = 0;
+ll eulerphi(ll n) {
+    ll result = 1;
+    for (ll i = 2; i <= n / i; i++) {
+        if (n % i == 0) {
+            while (n % i == 0) {
                 result *= i;
+                n /= i;
             }
+            result /= i;
             result *= (i - 1);
         }
     }
+    if (n > 1) {
+        result *= (n - 1);
+    }
+    return result;
 }
+
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    for (int i = 2; i * i <= 1000000; i++) {
-        if (!visited[i]) {
-            for (int j = i * i; j <= 1000000; j += i) {
-                visited[j] = true;
-            }
-        }
-    }
-    ll n = 0;
     cin >> n;
-    ll temp = n;
-    for (int i = 2; i <= n / i && temp > 1; i++) {
-        while (!visited[i] && temp % i == 0) {
-            soinsoo[i]++;
-            temp /= i;
-        }
-    }
-    if (temp > 1) {
-        result *= (temp - 1);
-    }
-    eulerphi(n);
-    cout << result;
+    cout << eulerphi(n);
     return 0;
 }
