573bc7155c81a0c89818f536cb26434593d342f4
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-02-20 15:09:09 +0900

    [Gold V] Title: 피타고라스 정리의 증명, Time: 344 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/33466. 피타고라스 정리의 증명/피타고라스 정리의 증명.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<ll, string> pls;
+typedef pair<string, ll> psl;
+typedef tuple<ll, ll, ll>tp;
+typedef tuple<pll, string, ll>pllsl;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll num = 0;
+    cin >> num;
+    while (num--) {
+        cin >> n;
+        cout << n + 2 * (n / 2) << '\n';
+    }
+    return 0;
+}
