56c85b4ff20ee1b36afc39f57b45b73d6c8e02ed
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-03-17 13:10:29 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> tp;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    double a = 0;
+    cin >> a;
+    cout << (ll)a;
+    return 0;
+}
