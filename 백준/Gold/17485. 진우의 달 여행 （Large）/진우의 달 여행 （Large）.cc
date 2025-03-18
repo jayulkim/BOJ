8a2833bf908f71194d8af551eb2fdcaf6b8e6fe5
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-03-18 19:47:43 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 240 ms, Memory: 64764 KB -BaekjoonHub

---
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> tp;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+vector<vector<vector<ll>>>dp(1001, vector<vector<ll>>(1001, vector<ll>(3, -1)));
+ll Map[1001][1001];
+ll dfs(ll a, ll b, ll c) {
+	if (b < 0 || b >= m) {
+		return 1e9;
+	}
+	if (a >= n) {
+		return 0;
+	}
+	if (dp[a][b][c] != -1) {
+		return dp[a][b][c];
+	}
+	ll weight = LLONG_MAX;
+	if (!c) {
+		weight = min({ weight, dfs(a + 1, b - 1, 1) + Map[a][b], dfs(a + 1, b + 1, 2) + Map[a][b] });
+	}
+	else if (c == 1) {
+		weight = min({ weight, dfs(a + 1, b, 0) + Map[a][b], dfs(a + 1, b + 1, 2) + Map[a][b] });
+	}
+	else if (c == 2) {
+		weight = min({ weight, dfs(a + 1, b, 0) + Map[a][b], dfs(a + 1, b - 1, 1) + Map[a][b] });
+	}
+	return dp[a][b][c] = weight;
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> Map[i][j];
+		}
+	}
+	ll Min = LLONG_MAX;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < 3; j++) {
+			Min = min(Min, dfs(0, i, j));
+		}
+	}
+	cout << Min;
+	return 0;
+}
