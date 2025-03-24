979f24d8ebd4f51c6fa80edbdf02323c15a9669d
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-03-24 22:46:57 +0900

    [Gold IV] Title: 합이 0, Time: 32 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -5,7 +5,7 @@ typedef long double ld;
 typedef tuple<ll, ll, ll> lll;
 typedef pair<ll, ll> pll;
 ll n = 0, m = 0, k = 0;
-
+ll visited[50001];
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
@@ -16,13 +16,12 @@ int main(void) {
 		cin >> num;
 		v.push_back(num);
 	}
-	sort(v.begin(), v.end());
 	ll count = 0;
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
-			ll a = lower_bound(v.begin() + j + 1, v.end(), -(v[i] + v[j])) - v.begin(), b = upper_bound(v.begin() + j + 1, v.end(), -(v[i] + v[j])) - v.begin();
-			count += b - a;
+			count += visited[-(v[i] + v[j]) + 25000];
 		}
+		visited[v[i] + 25000]++;
 	}
 	cout << count;
 	return 0;
