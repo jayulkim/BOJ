b3e5dcb2f7ea16dbfec380ad8fb47dfb34c3166f
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-04-09 06:32:29 +0900

    [Platinum IV] Title: 계단 보행, Time: 612 ms, Memory: 90368 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/33758. 계단 보행/계단 보행.cc
+++ 백준/Platinum/33758. 계단 보행/계단 보행.cc
@@ -6,42 +6,83 @@ typedef tuple<ll, ll, ll> lll;
 typedef tuple<ll, ll, ll, ll> llll;
 typedef pair<ll, ll> pll;
 ll n = 0, m = 0, k = 0;
-vector<pll>graph[50001];
-ll visited[50001];
-vector<ll>result(50001, -1);
-void bfs(ll start) {
-    queue<lll>q;
-    q.push({ start, 0, 0 });
-    while (!q.empty()) {
-        auto [a, b, c] = q.front();
-        q.pop();
-        visited[a]++;
-        if ((a == start && visited[a] == 2) || (a != start && visited[a] == 1)) {
-            result[a] = c;
-        }
-        for (auto& i : graph[a]) {
-            if (a == start && visited[a] < n) {
-                q.push({ i.first, i.second, c + 1 });
-            }
-            else if (visited[i.first] < n && llabs(b - i.second) == 1) {
-                q.push({ i.first, i.second, c + 1 });
-            }
-        }
-    }
-}
+vector<vector<ll>>graphgraph(1000001);
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> n >> m;
+    vector<vector<pll>>graph(n + 1);
+    vector<map<ll, ll>>Map(n + 1);
+    map<pll, ll>v;
+    map<ll, ll>vv;
+    ll idx = 0;
     for (int i = 0; i < m; i++) {
         ll a = 0, b = 0, c = 0;
         cin >> a >> b >> c;
-        graph[a].push_back({ b, c });
-        graph[b].push_back({ a, c });
+        graph[a].push_back({ c, b });
+        graph[b].push_back({ c, a });
+        Map[a][c]++;
+        Map[b][c]++;
     }
-    bfs(1);
+    v[{1, 0}] = ++idx;
+    queue<pll>q;
+    q.push({ v[{1, 0}], 0});
+    for (auto& i : graph[1]) {
+        if (!v[{i.second, i.first}]) {
+            v[{i.second, i.first}] = ++idx;
+            vv[idx] = i.second;
+        }
+        graphgraph[v[{1, 0}]].push_back(v[{i.second, i.first}]);
+        q.push({ v[{ i.second, i.first }], 1 });
+    }
+    for (int i = 2; i <= n; i++) {
+        for (auto& j : graph[i]) {
+            if (j.first + 1 <= 100000 && Map[i][j.first + 1]) {
+                if (!v[{i, j.first + 1}]) {
+                    v[{i, j.first + 1}] = ++idx;
+                    vv[idx] = i;
+                }
+                if (!v[{ j.second, j.first }]) {
+                    v[{ j.second, j.first }] = ++idx;
+                    vv[idx] = j.second;
+                }
+                graphgraph[v[{i, j.first + 1}]].push_back(v[{ j.second, j.first }]);
+            }
+            if (j.first - 1 >= 1 && Map[i][j.first - 1]) {
+                if (!v[{i, j.first - 1}]) {
+                    v[{i, j.first - 1}] = ++idx;
+                    vv[idx] = i;
+                }
+                if (!v[{ j.second, j.first }]) {
+                    v[{ j.second, j.first }] = ++idx;
+                    vv[idx] = j.second;
+                }
+                graphgraph[v[{i, j.first - 1}]].push_back(v[{ j.second, j.first }]);
+            }
+        }
+    }
+    vector<bool>visited(idx + 1, false);
+    vector<ll>result(n + 1, LLONG_MAX);
+    while (!q.empty()) {
+        pll temp = q.front();
+        q.pop();
+        if (!visited[temp.first]) {
+            result[vv[temp.first]] = min(result[vv[temp.first]], temp.second);
+            visited[temp.first] = true;
+            for (auto& i : graphgraph[temp.first]) {
+                if (!visited[i]) {
+                    q.push({ i, temp.second + 1 });
+                }
+            }
+        }
+    }
     for (int i = 1; i <= n; i++) {
-        cout << result[i] << ' ';
+        if (result[i] == LLONG_MAX) {
+            cout << -1 << ' ';
+        }
+        else {
+            cout << result[i] << ' ';
+        }
     }
-    return 0;   
+    return 0;
 }
