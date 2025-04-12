37828727a95191e30c14518e7f2c9a7a4dbac799
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-04-12 20:29:54 +0900

    [Platinum II] Title: 마계안암, Time: 328 ms, Memory: 51908 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/27934. 마계안암/마계안암.cc
+++ 백준/Platinum/27934. 마계안암/마계안암.cc
@@ -13,38 +13,35 @@ int main(void) {
     cin >> n >> m;
     vector<vector<pll>>graph(n + 1);
     vector<vector<ll>>topograph(n + 1);
-    vector<vector<ll>>dijkgraph(n + 1);
+    vector<vector<pll>>dijkgraph(n + 1);
     vector<ll>degree(n + 1, 0);
     priority_queue<pll, vector<pll>, greater<pll>>pq;
     vector<ll>dist(n + 1, LLONG_MAX);
-    vector<ll>reversedist(n + 1, LLONG_MAX);
     vector<ll>s;
     map<ll, ll>Map;
     map<ll, ll>infMap;
     ll id = 0;
     ll Mapid = 0;
     vector<ll>visited(n + 1, 0);
-    vector<vector<ll>>parent(n + 1);
     vector<bool>finished(n + 1, false);
-    map<pll, ll>zero;
+    vector<lll>v;
+    vector<vector<ll>>scc;
     for (int i = 0; i < m; i++) {
         ll a = 0, b = 0, c = 0;
         cin >> a >> b >> c;
         graph[a].push_back({ c, b });
-        if (!c) {
-            zero[{a, b}]++;
-        }
+        v.push_back({ a, b, c });
     }
     function<ll(ll)> dfs = [&](ll start) -> ll {
         visited[start] = ++id;
         s.push_back(start);
         ll parent = visited[start];
         for (auto& i : dijkgraph[start]) {
-            if (!visited[i]) {
-                parent = min(parent, dfs(i));
+            if (!visited[i.first]) {
+                parent = min(parent, dfs(i.first));
             }
-            else if (!finished[i]) {
-                parent = min(parent, visited[i]);
+            else if (!finished[i.first]) {
+                parent = min(parent, visited[i.first]);
             }
         }
         if (parent == visited[start]) {
@@ -60,16 +57,7 @@ int main(void) {
                     break;
                 }
             }
-            bool find = false;
-            for (int i = 0; i < v.size(); i++) {
-                if (!zero[{v[(i + 1) % v.size()], v[i]}]) {
-                    find = true;
-                    break;
-                }
-            }
-            if (!find) {
-                infMap[Mapid]++;
-            }
+            scc.push_back(v);
         }
         return parent;
         };
@@ -83,21 +71,18 @@ int main(void) {
                 if (dist[i.second] > dist[temp.second] + i.first) {
                     dist[i.second] = dist[temp.second] + i.first;
                     pq.push({ dist[i.second], i.second });
-                    vector<ll>().swap(parent[i.second]);
-                    parent[i.second].push_back(temp.second);
-                }
-                else if (dist[i.second] == dist[temp.second] + i.first) {
-                    parent[i.second].push_back(temp.second);
                 }
             }
         }
     }
-    for (int i = 2; i <= n; i++) {
-        for (auto& j : parent[i]) {
-            dijkgraph[j].push_back(i);
-            if (zero[{i, j}]) {
-                dijkgraph[i].push_back(j);
-            }
+    vector<lll>vvv;
+    for (auto& [a, b, c] : v) {
+        if (dist[b] == dist[a] + c) {
+            dijkgraph[a].push_back({ b, c });
+            vvv.push_back({ a, b, c });
+        }
+        if (a == b && !c) {
+            infMap[Map[a]]++;
         }
     }
     for (int i = 1; i <= n; i++) {
@@ -105,15 +90,51 @@ int main(void) {
             dfs(i);
         }
     }
+    for (auto& [a, b, c] : vvv) {
+        if (a == b && !c) {
+            infMap[Map[a]]++;
+        }
+    }
+    vector<bool>visitedvisited(n + 1, false);
+    function<ll(ll)> bfs = [&](ll start) -> ll {
+        ll result = 0;
+        queue<ll>q;
+        visitedvisited[start] = true;
+        q.push(start);
+        while (!q.empty()) {
+            ll temp = q.front();
+            q.pop();
+            visitedvisited[temp] = true;
+            for (auto& i : dijkgraph[temp]) {
+                if (!visitedvisited[i.first] && Map[temp] == Map[i.first]) {
+                    visitedvisited[i.first] = true;
+                    q.push(i.first);
+                    result += i.second;
+                }
+            }
+        }
+        return result;
+        };
+    for (int i = 1; i <= Mapid; i++) {
+        if (!bfs(scc[i - 1][0]) && scc[i - 1].size() > 1) {
+            infMap[i]++;
+        }
+    }
     for (int i = 1; i <= n; i++) {
         ll d = Map[i];
         for (auto& j : dijkgraph[i]) {
-            if (d != Map[j]) {
-                topograph[d].push_back(Map[j]);
-                degree[Map[j]]++;
+            if (d != Map[j.first]) {
+                topograph[d].push_back(Map[j.first]);
+                degree[Map[j.first]]++;
             }
         }
     }
+    function<ll(ll, ll)> addmod = [&](ll x, ll y) -> ll {
+        if (x >= 998244353 - y) {
+            return x - (998244353 - y);
+        }
+        return x + y;
+        };
     vector<ll>dp(n + 1, 0);
     vector<bool>vv(n + 1, false);
     queue<ll>q;
@@ -140,9 +161,10 @@ int main(void) {
                 dp[i] = -1;
             }
             else if (vv[temp]) {
-                dp[i] += dp[temp];
-                dp[i] %= 998244353;
-                vv[i] = true;
+                if (dp[i] != -1) {
+                    dp[i] = addmod(dp[i], dp[temp]);
+                    vv[i] = true;
+                }
             }
         }
     }
