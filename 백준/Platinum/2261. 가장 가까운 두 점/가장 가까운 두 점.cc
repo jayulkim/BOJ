63be8f02570d6aa09d7fb0b3b848170e8c14d2ed
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-04-17 16:52:34 +0900

    [Platinum II] Title: 가장 가까운 두 점, Time: 80 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Platinum/2261. 가장 가까운 두 점/가장 가까운 두 점.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+vector<pll>v;
+ll getdist(pll a, pll b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+ll dfs(ll start, ll end) {
+    if (start == end) {
+        return 1e20;
+    }
+    if (end - start <= 2) {
+        ll result = LLONG_MAX;
+        for (int i = start; i + 1 <= end; i++) {
+            for (int j = i + 1; j <= end; j++) {
+                result = min(result, getdist(v[i], v[j]));
+            }
+        }
+        return result;
+    }
+    ll mid = (start + end) / 2;
+    ll a = dfs(start, mid);
+    ll b = dfs(mid + 1, end);
+    ll d = min(a, b);
+    ll c = LLONG_MAX;
+    vector<pll>vv;
+    for (int i = mid; i >= start; i--) {
+        if ((v[mid].first - v[i].first) * (v[mid].first - v[i].first) >= d) {
+            break;
+        }
+        vv.push_back({ v[i].second, v[i].first });
+    }
+    for (int i = mid + 1; i <= end; i++) {
+        if ((v[mid].first - v[i].first) * (v[mid].first - v[i].first) >= d) {
+            break;
+        }
+        vv.push_back({ v[i].second, v[i].first });
+    }
+    sort(vv.begin(), vv.end());
+    for (int i = 0; i + 1 < vv.size(); i++) {
+        for (int j = i + 1; j < vv.size(); j++) {
+            if ((vv[j].first - vv[i].first) * (vv[j].first - vv[i].first) >= d) {
+                break;
+            }
+            c = min(c, getdist(vv[i], vv[j]));
+        }
+    }
+    return min(c, d);
+}
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        ll a = 0, b = 0;
+        cin >> a >> b;
+        v.push_back({ a, b });
+    }
+    sort(v.begin(), v.end());
+    cout << dfs(0, n - 1);
+    return 0;
+}
