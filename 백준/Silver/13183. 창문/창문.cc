032efc59f4ede13af3e516a4c50c9f670cf63b52
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-05-18 03:58:23 +0900

    [Silver I] Title: 창문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13183. 창문/창문.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef pair<ll, string> pls;
+typedef pair<string, ll> psl;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<pll, string, ll> pllsl;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	cout << (((n + 2) % 1000000007) * ((m + 2) % 1000000007)) % 1000000007;
+	return 0;
+}
