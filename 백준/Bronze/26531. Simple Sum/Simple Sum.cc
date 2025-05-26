a9e821cfcd8db6dbf402cbfa64f3bda46640960f
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-05-26 19:12:51 +0900

    [Bronze IV] Title: Simple Sum, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string a = "", b = "", c = "", d = "", e = "";
+    cin >> a >> b >> c >> d >> e;
+    if (stoll(a) + stoll(c) == stoll(e)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
