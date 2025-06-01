a70a54bd3ce3054a1b037fb42addc9ffc9838295
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-06-02 07:23:58 +0900

    [Silver IV] Title: 트리 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> m;
+    ll count = 0;
+    for (int i = 0; i < n - m; i++) {
+        cout << count++ << ' ' << count << '\n';
+    }
+    ll temp = count++;
+    for (int i = 0; i < m - 1; i++) {
+        cout << temp << ' ' << count++ << '\n';
+    }
+    return 0;
+}
