0103bd4d5bd8e579cdbcca47b2a5f5032252f8fb
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-06-21 02:14:36 +0900

    [Bronze IV] Title: MBTI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25640. MBTI/MBTI.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string str = "";
+    cin >> str;
+    ll count = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string temp = "";
+        cin >> temp;
+        if (str == temp) {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
