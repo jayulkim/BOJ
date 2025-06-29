f6c099617cc920c4aa66f42a72747e488e26a76b
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-06-30 04:17:10 +0900

    [Bronze III] Title: 피드백, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18856. 피드백/피드백.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    cout << n << '\n';
+    for (int i = 1; i <= n - 1; i++) {
+        cout << i << ' ';
+    }
+    cout << 199;
+    return 0;
+}
