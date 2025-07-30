c439874609a926becb612cc4a6807b4617004c61
jayulkim <144874950+jayulkim@users.noreply.github.com>
2025-07-31 01:24:58 +0900

    [Bronze III] Title: Dominos, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15236. Dominos/Dominos.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+ll n = 0, m = 0, k = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    cout << n * (n + 1) * (n + 2) / 2;
+    return 0;
+ } 
