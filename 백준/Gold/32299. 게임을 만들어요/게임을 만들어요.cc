98f983e6b519de6f463ad4353871584af8ddcac5
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-03-15 23:20:26 +0900

    [Gold III] Title: 게임을 만들어요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/32299. 게임을 만들어요/게임을 만들어요.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ld, ld> pld;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n = 0;
+	cin >> n;
+	if (n == 3) {
+		cout << "Hobanwoo";
+	}
+	else {
+		cout << "Sangho";
+	}
+	return 0;
+}
