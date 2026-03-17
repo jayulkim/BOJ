86ed7ab7badf11c5bc97d4d1995836ff70175f6b
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-03-17 12:00:19 +0900

    [Gold I] Title: 수영장 만들기, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Gold/1113. 수영장 만들기/수영장 만들기.cc
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.cc
@@ -19,70 +19,45 @@ int main(void) {
 		}
 	}
 	vector<vector<ll>>forward = { {-1, 0}, {0, -1}, {1, 0}, {0, 1} };
-	vector<vector<ll>>flood(n + 1, vector<ll>(m + 1, -1));
-	for (int i = 0; i < n; i++) {
+	vector<vector<ll>>vii(n + 1, vector<ll>(m + 1, 0));
+	ll result = 0;
+	for (int i = 9; i >= 2; i--) {
+		vector<vector<ll>>vi(n + 1, vector<ll>(m + 1, 0));
+		queue<pll>q;
+		for (int j = 0; j < n; j++) {
+			if (Map[j][0] - '0' < i) {
+				q.push({ j, 0 });
+			}
+			if (Map[j][m - 1] - '0' < i) {
+				q.push({ j, m - 1 });
+			}
+		}
 		for (int j = 0; j < m; j++) {
-			if (flood[i][j] == -1) {
-				vector<vector<ll>>vi(n + 1, vector<ll>(m + 1, 0));
-				bool find = false;
-				vector<pll>v;
-				queue<pll>q;
-				q.push({ i, j });
-				while (!q.empty()) {
-					auto [a, b] = q.front();
-					q.pop();
-					vi[a][b] = 1;
-					if (Map[a][b] == Map[i][j]) {
-						v.push_back({ a, b });
-					}
-					for (int k = 0; k < 4; k++) {
-						ll dy = a + forward[k][0];
-						ll dx = b + forward[k][1];
-						if (dy >= 0 && dy < n && dx >= 0 && dx < m) {
-							if (!vi[dy][dx] && Map[dy][dx] <= Map[i][j]) {
-								vi[dy][dx] = 1;
-								q.push({ dy, dx });
-							}
-						}
-						else {
-							find = true;
-						}
-					}
-				}
-				for (auto& [a, b] : v) {
-					flood[a][b] = find;
+			if (Map[0][j] - '0' < i) {
+				q.push({ 0, j });
+			}
+			if (Map[n - 1][j] - '0' < i) {
+				q.push({ n - 1, j });
+			}
+		}
+		while (!q.empty()) {
+			auto [a, b] = q.front();
+			q.pop();
+			vi[a][b] = 1;
+			for (int j = 0; j < 4; j++) {
+				ll dy = a + forward[j][0];
+				ll dx = b + forward[j][1];
+				if (dy >= 0 && dy < n && dx >= 0 && dx < m && !vi[dy][dx] && Map[dy][dx] - '0' < i) {
+					vi[dy][dx] = 1;
+					q.push({ dy, dx });
 				}
 			}
 		}
-	}
-	vector<vector<ll>>vii(n + 1, vector<ll>(m + 1, 0));
-	ll result = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (!flood[i][j] && !vii[i][j]) {
-				queue<pll>q;
-				vector<ll>v;
-				ll Min = LLONG_MAX;
-				q.push({ i, j });
-				while (!q.empty()) {
-					auto [a, b] = q.front();
-					q.pop();
-					vii[a][b] = 1;
-					v.push_back(Map[a][b]);
-					for (int k = 0; k < 4; k++) {
-						ll dy = a + forward[k][0];
-						ll dx = b + forward[k][1];
-						if (flood[dy][dx]) {
-							Min = min(Min, (ll)Map[dy][dx]);
-						}
-						if (!vii[dy][dx] && !flood[dy][dx]) {
-							vii[dy][dx] = 1;
-							q.push({ dy, dx });
-						}
-					}
-				}
-				for (auto& k : v) {
-					result += (Min - k);
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
+				if (!vii[j][k] && !vi[j][k]) {
+					vii[j][k] = i;
+					result += (i - Map[j][k] + '0');
 				}
 			}
 		}
