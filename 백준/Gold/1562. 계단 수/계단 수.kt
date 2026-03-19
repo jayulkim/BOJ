58a15af8135a205af072e9a7b2863273b40355d7
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-03-20 05:25:29 +0900

    [Gold I] Title: 계단 수, Time: 112 ms, Memory: 23088 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.kt
@@ -0,0 +1,46 @@
+import java.io.*
+import kotlin.math.*
+typealias ii = Int
+typealias ll = Long
+typealias ld = Double
+
+fun dfs(dp: Array<Array<LongArray>>, len: ii, temp: ii, prev: ii, end: ii, mod: ll): ll {
+    if (len == end) {
+        if (temp == (1 shl 10) - 1) {
+            return 1
+        }
+        return 0
+    }
+    if (dp[len][temp][prev] != -1L) {
+        return dp[len][temp][prev]
+    }
+    var weight = 0L
+    for (i in 0..9) {
+        if (abs(prev - i) == 1) {
+            weight += dfs(dp, len + 1, temp or (1 shl i), i, end, mod) % mod
+            weight %= mod
+        }
+    }
+    dp[len][temp][prev] = weight
+    return weight
+}
+
+fun solve() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = br.readLine().toInt()
+    val dp = Array(n) { Array(1 shl 10) { LongArray(10){ -1 } } }
+    var result = 0L
+    val mod = 1000000000L
+    for (i in 1..9) {
+        result += dfs(dp, 1, 1 shl i, i, n, mod)
+        result %= mod
+    }
+    bw.write("$result")
+    bw.flush()
+    bw.close()
+}
+
+fun main() {
+    solve()
+}
