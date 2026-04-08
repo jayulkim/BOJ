38074be97600d269c8ab35e327ce25d731f77fd3
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-04-08 14:54:48 +0900

    [Gold I] Title: 서로소 조합, Time: 232 ms, Memory: 197788 KB -BaekjoonHub

--- 백준/Gold/33916. 서로소 조합/서로소 조합.cc
+++ 백준/Gold/33916. 서로소 조합/서로소 조합.cc
@@ -12,18 +12,15 @@ ll n = 0, m = 0, k = 0;
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	vector<ll>prime;
-	vector<ll>vi(5001, 0);
-	for (int i = 2; i * i <= 5000; i++) {
-		if (!vi[i]) {
-			for (int j = i * i; j <= 5000; j += i) {
-				vi[j] = 1;
-			}
-		}
-	}
+	vector<vector<ll>>dp(5001, vector<ll>(5001, 0));
 	for (int i = 2; i <= 5000; i++) {
-		if (!vi[i]) {
-			prime.push_back(i);
+		dp[i] = dp[i - 1];
+		ll temp = i;
+		for (int j = 2; j <= temp && temp; j++) {
+			while (temp % j == 0) {
+				dp[i][j]++;
+				temp /= j;
+			}
 		}
 	}
 	ll num = 0;
@@ -31,43 +28,9 @@ int main(void) {
 	while (num--) {
 		ll a = 0, b = 0, c = 0, d = 0;
 		cin >> a >> b >> c >> d;
-		vector<ll>aup(5001, 0);
-		vector<ll>cup(5001, 0);
-		vector<ll>adown(5001, 0);
-		vector<ll>cdown(5001, 0);
-		for (auto& i : prime) {
-			for (int j = i; j <= a; j *= i) {
-				aup[i] += a / j;
-			}
-		}
-		for (auto& i : prime) {
-			for (int j = i; j <= c; j *= i) {
-				cup[i] += c / j;
-			}
-		}
-		for (auto& i : prime) {
-			for (int j = i; j <= b; j *= i) {
-				adown[i] += b / j;
-			}
-		}
-		for (auto& i : prime) {
-			for (int j = i; j <= a - b; j *= i) {
-				adown[i] += (a - b) / j;
-			}
-		}
-		for (auto& i : prime) {
-			for (int j = i; j <= d; j *= i) {
-				cdown[i] += d / j;
-			}
-		}
-		for (auto& i : prime) {
-			for (int j = i; j <= c - d; j *= i) {
-				cdown[i] += (c - d) / j;
-			}
-		}
 		ll result = 1;
 		for (int i = 2; i <= max(a, c); i++) {
-			if ((aup[i] - adown[i] > 0) && (cup[i] - cdown[i] > 0)) {
+			if ((dp[a][i] - dp[b][i] - dp[(a - b)][i] > 0) && (dp[c][i] - dp[d][i] - dp[(c - d)][i] > 0)) {
 				result = 0;
 				break;
 			}
