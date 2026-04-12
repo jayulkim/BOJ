f6e1c3844d1ab878b5e1ac0fd86016b05cca6202
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-04-13 08:14:14 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2228 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.cc
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -1,60 +1,78 @@
 #include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
 using namespace std;
-vector<int>graph[1001];
-int degree[1001];
-int stop = 0;
-vector<int>result;
-void toposort(int n) {
-	queue<int>q;
-	for (int i = 1; i <= n; i++) {
-		if (degree[i] == 0) {
-			q.push(i);
-		}
+typedef long long ll;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+typedef pair<string, ll> psl;
+ll n = 0, m = 0, k = 0;
+class Toposort {
+private:
+	using ll = long long;
+	ll n;
+	vector<vector<ll>>dag;
+public:
+	Toposort(ll n, const vector<vector<ll>>& dag) {
+		this->n = n;
+		this->dag = dag;
 	}
-	for (int i = 0; i < n; i++) {
-		if (!q.empty()) {
-			int temp = q.front();
+	pair<bool, vector<ll>> toposort() {
+		vector<ll>degree(n + 1, 0);
+		for (int i = 1; i <= n; i++) {
+			for (auto& j : dag[i]) {
+				degree[j]++;
+			}
+		}
+		vector<ll>result;
+		queue<ll>q;
+		for (int i = 1; i <= n; i++) {
+			if (!degree[i]) {
+				q.push(i);
+			}
+		}
+		while (!q.empty()) {
+			ll temp = q.front();
 			q.pop();
 			result.push_back(temp);
-			for (auto& j : graph[temp]) {
-				if (--degree[j] == 0) {
-					q.push(j);
+			for (auto& i : dag[temp]) {
+				if (--degree[i] == 0) {
+					q.push(i);
 				}
 			}
 		}
-		else {
-			cout << 0;
-			stop++;
-			return;
-		}
+		return { result.size() == n, result};
 	}
-}
+};
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int n = 0, m = 0;
 	cin >> n >> m;
+	vector<vector<ll>>graph(n + 1);
 	for (int i = 0; i < m; i++) {
-		int k = 0;
 		cin >> k;
-		int prev = 0;
+		ll prev = 0;
 		for (int j = 0; j < k; j++) {
-			int num = 0;
-			cin >> num;
-			if (j == 0) {
-				prev = num;
+			ll a = 0;
+			cin >> a;
+			if (!j) {
+				prev = a;
 			}
 			else {
-				graph[prev].push_back(num);
-				degree[num]++;
-				prev = num;
+				graph[prev].push_back(a);
+				prev = a;
 			}
 		}
 	}
-	toposort(n);
-	if (stop == 0) {
-		for (auto& i : result) {
-			cout << i << ' ';
+	Toposort t(n, graph);
+	pair<bool, vector<ll>>result = t.toposort();
+	if (!result.first) {
+		cout << 0;
+	}
+	else {
+		for (auto& i : result.second) {
+			cout << i << '\n';
 		}
 	}
 	return 0;
