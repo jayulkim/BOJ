5caf0e1002eba59c6c46f3ab440371991aed29f2
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-04-13 06:35:58 +0900

    [Platinum V] Title: 풍선줄 자르기, Time: 220 ms, Memory: 51580 KB -BaekjoonHub

--- 백준/Platinum/35473. 풍선줄 자르기/풍선줄 자르기.cc
+++ 백준/Platinum/35473. 풍선줄 자르기/풍선줄 자르기.cc
@@ -8,7 +8,70 @@ typedef tuple<ll, ll, ll, ll> llll;
 typedef pair<ll, ll> pll;
 typedef pair<string, ll> psl;
 ll n = 0, m = 0, k = 0;
-
+class Dijkstra {
+private:
+	using ll = long long;
+	using pll = pair<ll, ll>;
+	ll n;
+	vector<ll>start;
+	vector<vector<pll>>graph;
+public:
+	vector<ll>dist;
+	vector<vector<pll>>revgraph;
+	Dijkstra(ll n, vector<ll>start, vector<vector<pll>>&graph) {
+		this->n = n;
+		this->start = start;
+		this->graph = graph;
+	}
+	vector<ll> run() {
+		priority_queue<pll, vector<pll>, greater<pll>>pq;
+		dist.assign(n + 1, LLONG_MAX);
+		for (auto& i : start) {
+			dist[i] = 0;
+			pq.push({ 0, i });
+		}
+		while (!pq.empty()) {
+			auto [a, b] = pq.top();
+			pq.pop();
+			if (dist[b] >= a) {
+				for (auto& i : graph[b]) {
+					if (dist[i.first] > dist[b] + i.second) {
+						dist[i.first] = dist[b] + i.second;
+						pq.push({ dist[i.first], i.first });
+					}
+				}
+			}
+		}
+		return dist;
+	}
+	pair<vector<ll>, vector<vector<pll>>> revrun() {
+		priority_queue<pll, vector<pll>, greater<pll>>pq;
+		dist.assign(n + 1, LLONG_MAX);
+		revgraph.assign(n + 1, vector<pll>());
+		for (auto& i : start) {
+			dist[i] = 0;
+			pq.push({ 0, i });
+		}
+		while (!pq.empty()) {
+			auto [a, b] = pq.top();
+			pq.pop();
+			if (dist[b] >= a) {
+				for (auto& i : graph[b]) {
+					if (dist[i.first] > dist[b] + i.second) {
+						vector<pll>().swap(revgraph[i.first]);
+						revgraph[i.first].push_back({ b, i.second });
+						dist[i.first] = dist[b] + i.second;
+						pq.push({ dist[i.first], i.first });
+					}
+					else if (dist[i.first] == dist[b] + i.second) {
+						revgraph[i.first].push_back({ b, i.second });
+					}
+				}
+			}
+		}
+		return { dist, revgraph };
+	}
+};
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
@@ -20,33 +83,12 @@ int main(void) {
 		graph[a].push_back({ b, c });
 		graph[b].push_back({ a, c });
 	}
-	vector<vector<pll>>digraph(n + 1);
-	priority_queue<pll, vector<pll>, greater<pll>>pq;
-	vector<ll>dist(n + 1, LLONG_MAX);
-	vector<ll>redist(n + 1, LLONG_MAX);
-	pq.push({ 0, 0 });
-	dist[0] = 0;
-	while (!pq.empty()) {
-		auto [a, b] = pq.top();
-		pq.pop();
-		if (dist[b] >= a) {
-			for (auto& i : graph[b]) {
-				if (dist[i.first] > dist[b] + i.second) {
-					vector<pll>().swap(digraph[i.first]);
-					digraph[i.first].push_back({ b, i.second });
-					dist[i.first] = dist[b] + i.second;
-					pq.push({ dist[i.first], i.first });
-				}
-				else if (dist[i.first] == dist[b] + i.second) {
-					digraph[i.first].push_back({ b, i.second });
-				}
-			}
-		}
-	}
+	Dijkstra dijk(n, { 0 }, graph);
+	pair<vector<ll>, vector<vector<pll>>> v = dijk.revrun();
 	ll sum = 0;
 	for (int i = 0; i <= n; i++) {
 		ll Max = 0;
-		for (auto& j : digraph[i]) {
+		for (auto& j : v.second[i]) {
 			Max = max(Max, j.second);
 		}
 		sum += Max;
