fe5117b67c07dd3e5f8af4ef19e8a29efde97c4f
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-04-14 04:31:17 +0900

    [Diamond V] Title: gcd(n, k) = 1, Time: 0 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Diamond/13926. gcd（n， k） ＝ 1/gcd（n， k） ＝ 1.cc
@@ -0,0 +1,432 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef tuple<ll, ll, ll> lll;
+typedef tuple<ll, ll, ll, ll> llll;
+typedef pair<ll, ll> pll;
+typedef pair<string, ll> psl;
+ll n = 0, m = 0, k = 0;
+class Fastmod {
+private:
+	using ll = long long;
+	using ull = unsigned long long;
+	ull mod;
+	ull norm(ll x) {
+		ll m = (ll)mod;
+		x %= m;
+		if (x < 0) x += m;
+		return (ull)x;
+	}
+	ull addmod_u(ull a, ull b) {
+		if (a >= mod - b) {
+			return a - (mod - b);
+		}
+		return a + b;
+	}
+public:
+	Fastmod(ull mod) {
+		this->mod = mod;
+	}
+	ull addmod(ull a, ull b) {
+		return addmod_u(a, b);
+	}
+	ull addmod(ll a, ll b) {
+		return addmod_u(norm(a), norm(b));
+	}
+	ull fastmulmod(ull a, ull b) {
+		long long result = a * b - mod * (ull)(1.L / mod * a * b);
+		if (result < 0) {
+			result += mod;
+		}
+		if ((ull)result >= mod) {
+			result -= mod;
+		}
+		return (ull)result;
+	}
+	ull fastmulmod(ll a, ll b) {
+		return fastmulmod(norm(a), norm(b));
+	}
+	ull mulmod(ull a, ull b) {
+		ull result = 0;
+		while (b) {
+			if (b & 1) {
+				result = addmod_u(result, a);
+			}
+			a = addmod_u(a, a);
+			b >>= 1;
+		}
+		return result;
+	}
+	ull mulmod(ll a, ll b) {
+		return mulmod(norm(a), norm(b));
+	}
+	ull fastpowmod(ll a, ull b) {
+		ull x = norm(a);
+		ull result = 1;
+		while (b) {
+			if (b & 1) {
+				result = fastmulmod(result, x);
+			}
+			x = fastmulmod(x, x);
+			b >>= 1;
+		}
+		return result;
+	}
+	ull powmod(ll a, ull b) {
+		ull x = norm(a);
+		ull result = 1;
+		while (b) {
+			if (b & 1) {
+				result = mulmod(result, x);
+			}
+			x = mulmod(x, x);
+			b >>= 1;
+		}
+		return result;
+	}
+};
+class Primecheck {
+private:
+	using ull = unsigned long long;
+public:
+	bool bigisprime(ull x) {
+		static const ull sample[] = { 2, 325, 9375, 28178, 450775, 9780504, 1795265022 };
+		if (x < 2 || x % 6 % 4 != 1) {
+			return (x | 1) == 3;
+		}
+		Fastmod fm(x);
+		ull s = __builtin_ctzll(x - 1);
+		ull d = (x - 1) >> s;
+		for (ull a : sample) {
+			ull temp = fm.fastpowmod(a % x, d);
+			ull i = s;
+			while (temp != 1 && temp != x - 1 && a % x && i--) {
+				temp = fm.fastmulmod(temp, temp);
+			}
+			if (temp != x - 1 && i != s) {
+				return false;
+			}
+		}
+		return true;
+	}
+	bool isprime(ull x) {
+		if (x < 2) {
+			return false;
+		}
+		if (x % 2 == 0) {
+			return x == 2;
+		}
+		for (ull i = 3; i <= x / i; i += 2) {
+			if (x % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	vector<bool> eratosthenes(ull n) {
+		vector<bool>result(n + 1, false);
+		result[1] = true;
+		for (int i = 2; i * i <= n; i++) {
+			if (!result[i]) {
+				for (int j = i * i; j <= n; j += i) {
+					result[j] = true;
+				}
+			}
+		}
+		return result;
+	}
+};
+class Fastgcd {
+private:
+	using ll = long long;
+	using ull = unsigned long long;
+	using lll = tuple<ll, ll, ll>;
+public:
+	ull fastgcd(ull a, ull b) {
+		ull shift = 0;
+		if (a == 0) {
+			return b;
+		}
+		if (b == 0) {
+			return a;
+		}
+		shift = __builtin_ctzll(a | b);
+		a >>= __builtin_ctzll(a);
+		do {
+			b >>= __builtin_ctzll(b);
+			if (a > b) {
+				swap(a, b);
+			}
+			b -= a;
+		} while (b);
+		return a << shift;
+	}
+	lll eea(ll a, ll mod) {
+		ll r1 = a, r2 = mod;
+		ll s1 = 1, s2 = 0;
+		ll t1 = 0, t2 = 1;
+		while (r2) {
+			ll q = r1 / r2;
+			ll r = r1 - q * r2;
+			r1 = r2;
+			r2 = r;
+			ll s = s1 - q * s2;
+			s1 = s2;
+			s2 = s;
+			ll t = t1 - q * t2;
+			t1 = t2;
+			t2 = t;
+		}
+		return { r1, s1, t1 };
+	}
+};
+class Primefactor {
+private:
+	using ll = long long;
+	using ull = unsigned long long;   
+	using pull = pair<ull, ull>;
+	using pll = pair<ll, ll>;
+	Fastgcd fg;
+	Primecheck pc;
+public:
+	ull pollardrho(ull x) {
+		if (x % 2 == 0) {
+			return 2;
+		}
+		Fastmod fm(x);
+		auto f = [&](ull a) {
+			return (fm.fastmulmod(a, a) + 1) % x;
+			};
+		ull a = 0, b = 0;
+		ull t = 30;
+		ull prd = 2;
+		ull i = 1;
+		ull q = 0;
+		while (t++ % 40 || fg.fastgcd(prd, x) == 1) {
+			if (a == b) {
+				a = ++i;
+				b = f(a);
+			}
+			q = fm.fastmulmod(prd, (a > b ? a - b : b - a));
+			if (q) {
+				prd = q;
+			}
+			a = f(a);
+			b = f(f(b));
+			if (!prd) {
+				return x;
+			}
+		}
+		return fg.fastgcd(prd, x);
+	}
+	vector<ull> bigfactor(ull x) {
+		vector<ull>result;
+		vector<ull>v = { x };
+		while (!v.empty()) {
+			ull temp = v.back();
+			v.pop_back();
+			if (temp == 1) {
+				continue;
+			}
+			if (pc.bigisprime(temp)) {
+				result.push_back(temp);
+				continue;
+			}
+			ull d = pollardrho(temp);
+			v.push_back(d);
+			v.push_back(temp / d);
+		}
+		return result;
+	}
+	void bigfactor(ull x, vector<ull>& result) {
+		vector<ull>v = { x };
+		while (!v.empty()) {
+			ull temp = v.back();
+			v.pop_back();
+			if (temp == 1) {
+				continue;
+			}
+			if (pc.bigisprime(temp)) {
+				result.push_back(temp);
+				continue;
+			}
+			ull d = pollardrho(temp);
+			v.push_back(d);
+			v.push_back(temp / d);
+		}
+	}
+	vector<ull> factor(ull x) {
+		vector<ull> result;
+		for (ull i = 2; i <= x / i; i++) {
+			while (x % i == 0) {
+				result.push_back(i);
+				x /= i;
+			}
+		}
+		if (x > 1) {
+			result.push_back(x);
+		}
+		return result;
+	}
+	void factor(ull x, vector<ull>& result) {
+		for (ull i = 2; i <= x / i; i++) {
+			while (x % i == 0) {
+				result.push_back(i);
+				x /= i;
+			}
+		}
+		if (x > 1) {
+			result.push_back(x);
+		}
+	}
+	vector<pll> factorialfactor(ull n, const vector<ll>& prime) {
+		vector<pll>result;
+		for (auto& i : prime) {
+			ull count = 0;
+			for (ull j = i; j <= n;) {
+				count += n / j;
+				if (j > n / i) {
+					break;
+				}
+				j *= i;
+			}
+			if (count > 0) {
+				result.push_back({ i, count });
+			}
+		}
+		return result;
+	}
+	void factorialfactor(ull n, const vector<ll>& prime, vector<ll>& result) {
+		for (auto& i : prime) {
+			ull count = 0;
+			for (ull j = i; j <= n;) {
+				count += n / j;
+				if (j > n / i) {
+					break;
+				}
+				j *= i;
+			}
+			if (count > 0) {
+				result[i] += count;
+			}
+		}
+	}
+	vector<ull> getbigdivisor(ull n) {
+		vector<ull>f = bigfactor(n);
+		sort(f.begin(), f.end());
+		vector<pull>pf;
+		for (auto& i : f) {
+			if (pf.empty() || pf.back().first != i) {
+				pf.push_back({ i, 1 });
+			}
+			else {
+				pf.back().second++;
+			}
+		}
+		vector<ull>result = { 1 };
+		for (auto& [a, b] : pf) {
+			size_t size = result.size();
+			ull temp = 1;
+			for (int i = 1; i <= b; i++) {
+				temp *= a;
+				for (size_t j = 0; j < size; j++) {
+					result.push_back(result[j] * temp);
+				}
+			}
+		}
+		return result;
+	}
+	void getbigdivisor(ull n, vector<ull>& result) {
+		vector<ull>f = bigfactor(n);
+		sort(f.begin(), f.end());
+		vector<pull>pf;
+		for (auto& i : f) {
+			if (pf.empty() || pf.back().first != i) {
+				pf.push_back({ i, 1 });
+			}
+			else {
+				pf.back().second++;
+			}
+		}
+		vector<ull>v = { 1 };
+		for (auto& [a, b] : pf) {
+			size_t size = v.size();
+			ull temp = 1;
+			for (int i = 1; i <= b; i++) {
+				temp *= a;
+				for (size_t j = 0; j < size; j++) {
+					v.push_back(v[j] * temp);
+				}
+			}
+		}
+		for (auto& i : v) {
+			result.push_back(i);
+		}
+	}
+	vector<ull> getdivisor(ull n) {
+		vector<ull> result;
+		for (ull i = 1; i <= n / i; i++) {
+			if (n % i == 0) {
+				result.push_back(i);
+				if (i != n / i) {
+					result.push_back(n / i);
+				}
+			}
+		}
+		return result;
+	}
+	void getdivisor(ull n, vector<ull>& result) {
+		for (ull i = 1; i <= n / i; i++) {
+			if (n % i == 0) {
+				result.push_back(i);
+				if (i != n / i) {
+					result.push_back(n / i);
+				}
+			}
+		}
+	}
+};
+class Eulerphi {
+private:
+	using ull = unsigned long long;
+	Primefactor pf;
+public:
+	ull eulerphi(ull n) {
+		ull result = 1;
+		for (ull i = 2; i <= n / i; i++) {
+			if (n % i == 0) {
+				while (n % i == 0) {
+					result *= i;
+					n /= i;
+				}
+				result /= i;
+				result *= (i - 1);
+			}
+		}
+		if (n > 1) {
+			result *= (n - 1);
+		}
+		return result;
+	}
+	ull bigeulerphi(ull n) {
+		vector<ull> fac = pf.bigfactor(n);
+		sort(fac.begin(), fac.end());
+		fac.erase(unique(fac.begin(), fac.end()), fac.end());
+		ull result = n;
+		for (ull p : fac) {
+			result = result / p * (p - 1);
+		}
+		return result;
+	}
+};
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n;
+	Eulerphi ep;
+	cout << ep.bigeulerphi(n);
+	return 0;
+}
